58d798f9c3d87f8338174261a56a26c9b508fc7b
MrStrelow <mathias.cammerlander@gmail.com>
2024-03-28 10:44:04 +0100

    le test

--- IdeaProjects/current lesson/LoginDemo/src/main/java/com/example/logindemo/datalayer/UserConnection.java
+++ IdeaProjects/current lesson/LoginDemo/src/main/java/com/example/logindemo/datalayer/UserConnection.java
@@ -1,4 +1,21 @@
 package com.example.logindemo.datalayer;
 
+import java.io.File;
+import java.io.IOException;
+
 public class UserConnection {
+    private File database;
+
+    public UserConnection() {
+        String fileName = "myDataBase.txt";
+        database = new File(fileName);
+
+        try {
+
+            database.createNewFile();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
