5a6dc200f22a16c1bee6f5706f739d55623e9a21
MrStrelow <mathias.cammerlander@gmail.com>
2024-03-28 10:30:27 +0100

    le test

--- IdeaProjects/current lesson/LoginDemo/src/main/java/com/example/logindemo/DTO/User.java
+++ IdeaProjects/current lesson/LoginDemo/src/main/java/com/example/logindemo/DTO/User.java
@@ -1,4 +1,27 @@
 package com.example.logindemo.DTO;
 
 public class User {
+    private String userName; // irgendwas mit dem wir suche koennen -> eine ID
+    private String passwort;
+
+    public User(String userName, String passwort) {
+        this.userName = userName;
+        this.passwort = passwort;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
 }
