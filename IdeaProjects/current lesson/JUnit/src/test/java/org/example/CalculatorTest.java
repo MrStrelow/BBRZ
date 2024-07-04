3e355adaf5b751368de3a8146b4b12ecc0d54772
MrStrelow <mathias.cammerlander@gmail.com>
2024-07-04 10:34:49 +0200

    le angabe aendero

--- IdeaProjects/current lesson/JUnit/src/test/java/org/example/CalculatorTest.java
+++ IdeaProjects/current lesson/JUnit/src/test/java/org/example/CalculatorTest.java
@@ -10,14 +10,21 @@ public class CalculatorTest {
 
     // Erstelle ein Objekt Calculator als Attribut der Klasse CalculatorTest
 
-    // Wir verwenden nun die methode "asserEquals" von JUnit (unsere Testing Library).
+    // Wir verwenden nun die methode "assertEquals" von JUnit (unsere Testing Library).
     // Diese Methode hat 2 Argumente.
     // - das 1. ist das Ergebnis, welches wir erwarten und
     // - das 2. ist der Aufruf der Methode welche zu testen ist.
     // TODO: Erstelle eine Methode welche public ist und testAdd heißt.
+    //  Diese hat keine argumente hat und void als rückgabe hat. Schreibe dort @Test darüber.
+
     // TODO: Erstelle eine Methode welche public ist und testSubtract heißt.
+    //  Diese hat keine argumente hat und void als rückgabe hat. Schreibe dort @Test darüber.
+
     // TODO: Erstelle eine Methode welche public ist und testMultiply heißt.
+    //  Diese hat keine argumente hat und void als rückgabe hat. Schreibe dort @Test darüber.
+
     // TODO: Erstelle eine Methode welche public ist und testDivide heißt.
+    //  Diese hat keine argumente hat und void als rückgabe hat. Schreibe dort @Test darüber.
 
     // Um testen zu können ob eine Exception geworfen wird ist die Methode "assertThrows" anwendbar.
     // Erstelle dazu eine "Anonyme Klasse" Executable executable = new Executable() {...};.
@@ -28,15 +35,16 @@ public class CalculatorTest {
     // AssertThrows
     // Optional: verwende noch assertEquals(...); um den Text der Geworfenen Exception abzufragen.
     // TODO: Erstelle eine Methode welche public ist und testDivideByZero heißt.
+    //  Diese hat keine argumente hat und void als rückgabe hat. Schreibe dort @Test darüber.
 
     // Versuche nun diese Methode als Lambda Ausdruck zu implementieren
     // (Intellij schlägt es dir vor, versuche zu verstehen was der Unterschied ist).
     // TODO: Erstelle eine Methode welche public ist und testDivideByZeroWithLambda heißt.
+    //  Diese hat keine argumente hat und void als rückgabe hat. Schreibe dort @Test darüber.
 
     // Um einen Rückgabewert einer Methode welcher True oder False sein kann überprüfen zu können verwenden wir
-    // assertTrue(...); bzw. assertFalse(...);. Diese MEthode hat ein Argument welche der Aufruf der Methode ist.
+    // assertTrue(...); bzw. assertFalse(...);. Diese Methode hat ein Argument welche der Aufruf der Methode ist.
     // Wenn wir erwarten, dass diese wahr zurückgeben soll, dann verwenden wir assertTrue, ansonsten assertFalse.
     // TODO: Erstelle eine Methode welche public ist und testIstTeilbarDurch heißt.
-
-
+    //  Diese hat keine argumente hat und void als rückgabe hat. Schreibe dort @Test darüber.
 }
