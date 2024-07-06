cb05cf9c5a0f360cf6952e37d65fcce82437e38c
MrStrelow <mathias.cammerlander@gmail.com>
2024-07-06 13:12:41 +0200

    le new folder

--- IdeaProjects/current lesson/JUnit/src/main/java/org/example/solution/mocked/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.solution.mocked;
-
-public interface UserRepository {
-    public User findById(String id);
-    public void save(User user);
-    public boolean isAvailable();
-}
