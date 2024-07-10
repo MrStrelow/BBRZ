39b166a3b0e827df52bf326e831870f643ea05bb
MrStrelow <mathias.cammerlander@gmail.com>
2024-07-10 10:34:07 +0200

    wresdfsdf

--- IdeaProjects/current lesson/p1007/src/Main.java
+++ IdeaProjects/current lesson/p1007/src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
 
+        System.out.println("Gib erstes ein: ");
+        Integer meinErsterInput = myScanner.nextInt();
+        String meinZweiterInput = myScanner.nextLine();
+
+        System.out.println("Deine erste Eingabe war " + meinErsterInput + " - deine zweite Eingabe war " + meinZweiterInput + ".");
+
+        System.out.println("gib bitte eine Zeile ein: ");
+        String meinDritterInput = myScanner.nextLine();
+        System.out.println(meinDritterInput);
     }
 }
--- IdeaProjects/lessons/block_1_basic_concepts/src/L05UserInput.java
+++ IdeaProjects/lessons/block_1_basic_concepts/src/L05UserInput.java
@@ -17,7 +17,7 @@ public class L05UserInput {
         // Wir verwenden das Keyword "new" um eine neue Variable anzulegen, gefolgt vom Typ der Variable.
         // In den Klammern können wir noch nähere Spezifikationen vollziehen (Für jene welche schon Objektorientiert programmiert haben,
         // hier legen wir ein Objekt myScanner, an welches den Konstruktor der Klasse Scanner aufruft und diesem ein Argument System.in gibt.
-        // Das lässt vermuten, dass wir nicht nur die Console als "Ort der Eingabe" festelegen können, sondern auch z.B. ein Textfile, Word, usw.).
+        // Das lässt vermuten, dass wir nicht nur die Console als "Ort der Eingabe" festlegen können, sondern auch z.B. ein Textfile, Word, usw.).
         Scanner myScanner = new Scanner(System.in);
 
         // Wir können nun den User eine Eingabe tätigen lassen. Dies geschieht mit der Variable "myScanner" und mit dem Zugriffsoperator ".".
@@ -56,8 +56,9 @@ public class L05UserInput {
 
         System.out.println("meine Erste Eingabe: " + meineErsteEingabe + " - und meine zweite Eingabe: " + meineZweiteEingabe);
 
-        // Wir sehen nun was der Unterschied ist! nextLine liest solange bis ein \n kommt und next liest bis ein Leerzeichen kommt.
-        // Wir können aber spezifizieren was dieses Trennzeichen ist.
+        // Wir sehen nun, was der Unterschied ist! nextLine liest solange bis ein \n kommt (INKLUSIVE DEM \n!) und
+        // next liest bis ein Leerzeichen bzw. das ein \n kommt (EXKLUSIVE DEM \n!).
+        // Wir können spezifizieren, was dieses Trennzeichen ist.
 
     }
 }
