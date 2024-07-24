8824229d0b872896ecec4a201c19d38d7687b61f
MrStrelow <mathias.cammerlander@gmail.com>
2024-07-24 13:59:08 +0200

    weds

--- IdeaProjects/current lesson/p1707/src/Main.java
+++ IdeaProjects/current lesson/p1707/src/Main.java
@@ -4,234 +4,33 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Gib eine ganze Zahl (Integer) ein: ");
-        String userInput = myScanner.nextLine();
-        Integer userInputZahl = Integer.parseInt(userInput);
+        // 1 )
+        // Wir haben Wochentage [Montag, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag]:
+        //  - Wenn eine Integer-Variable, welche der User eingibt gleich 1 ist, dann gib "Montag" aus, andernfalls, wenn
+        //    die Integer-Variable gleich 2 ist "Dienstag", usw. bis, wenn die Integer-Variable gleich 7 ist, dann gib "Sonntag" aus.
+        //  - Wenn wir "Freitag" ausgeben wollen füge ":)" dem Wochentag hinzu. Wenn wir Montag ausgeben füge einen ":(" hinzu.
 
-        // WENN
-        //      der userInput gleich 5 ist
-        // DANN
-        //      verdopple den userInput und gib einen Text deiner Wahl aus.
+        // TODO: implement
 
-         /*
-                   ()
-                    \
-           x == 5    \
-                     ||
-         */
+        // 2 )
+        // Wir haben Wochentage [Montag, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag]:
+        //  - Wenn eine Integer-Variable, welche der User eingibt gleich 1 ist, dann gib "Montag" aus, andernfalls, wenn
+        //    die Integer-Variable gleich 2 ist "Dienstag", usw. bis, wenn die Integer-Variable gleich 7 ist, dann gib "Sonntag" aus.
+        //  - Wenn wir "Freitag" ausgeben wollen füge ":)" dem Wochentag hinzu. Wenn wir Montag ausgeben füge einen ":(" hinzu.
+        //  - Zusätzlich hat der Montag eine chance von 80% 5 mal ":(", also ":(:(:(:(:(" zum String "Montag :(" hinzuzufügen.
+        //  - Zusätzlich hat der Freitag eine chance von 30% 7 mal ":(", also ":):):):):):):)" zum String "Freitag :)" hinzuzufügen.
 
-        if (userInputZahl == 5) {
-            System.out.println("mal schauen ob es ausgeführt wird.");
-            userInputZahl *= 2;
-        }
+        // TODO: implement
 
-        System.out.println("Der User hat die Zahl " + userInputZahl + " eingegeben.");
+        // 3)
+        // Wir haben Wochentage [Montag, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag]:
+        //  - Wenn eine Integer-Variable, welche der User eingibt gleich 1 ist, dann gib "Montag" aus, andernfalls, wenn
+        //    die Integer-Variable gleich 2 ist "Dienstag", usw. bis, wenn die Integer-Variable gleich 7 ist, dann gib "Sonntag" aus.
+        //  - Wenn wir "Freitag" ausgeben wollen füge ":)" dem Wochentag hinzu. Wenn wir Montag ausgeben füge einen ":(" hinzu.
+        //  - Zusätzlich hat der Montag eine chance von 80% 5 mal ":(", also ":(:(:(:(:(" zum String "Montag :(" hinzuzufügen.
+        //  - Zusätzlich hat der Freitag eine chance von 30% 7 mal ":(", also ":):):):):):):)" zum String "Freitag :)" hinzuzufügen.
+        //  - Zusätzlich hat jeder Tag eine Chance von 1%, dass dieser einen Smiley ":)" hat.
 
-
-        // WENN
-        //      der userInput gleich 5 ist
-        // DANN
-        //      verdopple den userInput und gib einen Text deiner Wahl aus.
-        // ANSONSTEN
-        //      halbiere den userInput
-
-
-        /*        ()  ()
-                   \  /
-           x == 5   \/   ELSE
-                    ||
-         */
-
-        if (userInputZahl == 5) {
-            System.out.println("mal schauen ob es ausgeführt wird.");
-            userInputZahl *= 2;
-        } else {
-            userInputZahl /= 2;
-        }
-
-        // Wir haben Wochentage:
-        // Wenn eine Integer-Variable (userInput) gleich 1 ist, dann gib "Montag" aus, andernfalls, wenn
-        // die Integer-Variable gleich 2 "Dienstag", usw. wenn bis Integer-Variable gleich 7, dann gib "Sonntag" aus.
-        // Wenn wir "Freitag" ausgeben wollen füge ":)" dem Wochentag hinzu. Wenn wir Montag ausgeben füge einen ":(" hinzu.
-
-        // WENN
-        //      eine Integer-Variable gleich 1 ist
-        // DANN
-        //      gib "Montag" aus.
-        // ANDERNFALLS
-
-        /*
-                      ()  ()
-                       \  /
-                ()  x<5 \/ ELSE
-                 \      /
-                  \    / *
-                   \  /
-             x==1   \/   ELSE
-                    ||
-         */
-
-
-        if (userInputZahl == 1) {
-            System.out.println("Montag");
-        } else {
-
-            System.out.println("KEIN MONTAG");
-
-            if (userInputZahl == 2) {
-                System.out.println("Dienstag");
-            } else {
-                if (userInputZahl == 3) {
-                    System.out.println("Mittwoch");
-                } else {
-                    if (userInputZahl == 4) {
-                        System.out.println("Donnerstag");
-                    } else {
-                        if (userInputZahl == 5) {
-                            System.out.println("Freitag");
-                        } else {
-                            if (userInputZahl == 6) {
-                                System.out.println("Samstag");
-                            } else {
-                                System.out.println("Sonntag");
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        /*
-              ()    ()      ()
-         x==5  \ x<5 | ELSE/
-                \    |    /
-                 \   |   /
-                  \  |  /
-                   \ | /
-                    \|/
-                    | |
-         */
-
-        if (userInputZahl == 1) {
-            System.out.println("Montag");
-
-        } else if (userInputZahl == 2) {
-            System.out.println("Dienstag");
-
-        } else if (userInputZahl == 3) {
-            System.out.println("Mittwoch");
-
-        } else if (userInputZahl == 4) {
-            System.out.println("Donnerstag");
-
-        } else if (userInputZahl == 5) {
-            System.out.println("Freitag");
-
-        } else if (userInputZahl == 6) {
-            System.out.println("Samstag");
-
-        } else {
-            System.out.println("Sonntag");
-        }
-
-        // Übung:
-
-        // 1.)
-        // "WENN die Zahl die er User eingibt gleich 5 ist,
-        //  DANN gib 'mal schauen ob es ausgeführt wird'. aus.
-        //  ANSONSTEN gib ':(' aus und verdopple x"
-
-        if (userInputZahl == 5) {
-            System.out.println("mal schauen ob es ausgeführt wird");
-        } else {
-            System.out.println(":(");
-            userInputZahl *= 2;
-        }
-
-
-        // 2.)
-        // "WENN die Zahl die er User eingibt gleich 5 ist,
-        //  DANN gib 'mal schauen ob es ausgeführt wird'. aus.
-        //  ANSONSTEN
-        //      WENN die Zahl die er User eingibt kleiner 5 ist
-        //      DANN gib ':)' aus und halbiere x"
-        //      ANSONSTEN gib ':(' aus und verdopple x".
-
-        if (userInputZahl == 5) {
-
-            System.out.println("mal schauen ob es ausgeführt wird");
-
-        } else {
-
-            if (userInputZahl < 5) {
-
-                System.out.println(":)");
-                userInputZahl /= 2;
-
-            } else {
-
-                System.out.println(":(");
-                userInputZahl *= 2;
-
-            }
-
-        }
-
-        // 3.)
-        // "WENN die Zahl die er User eingibt UNgleich 5 ist,
-        //  DANN
-        //      WENN die Zahl die er User eingibt kleiner 5 ist
-        //      DANN gib ':)' aus und halbiere x"
-        //      ANSONSTEN gib ':(' aus und verdopple x".
-        //  ANSONSTEN
-        //  gib 'mal schauen ob es ausgeführt wird'. aus.
-
-        if (userInputZahl != 5) {
-
-           if (userInputZahl < 5) {
-               System.out.println(":)");
-               userInputZahl /= 2;
-           } else {
-               System.out.println(":(");
-               userInputZahl *= 2;
-           }
-
-        } else {
-
-            System.out.println("mal schauen ob es ausgeführt wird");
-
-        }
-
-
-        // 4.)
-        // "WENN die Zahl die er User eingibt gleich 5 ist,
-        //  DANN weise einem String 'user hat den 5er eingegeben'.
-        //  ANSONSTEN weise den gleichen String 'user hat nicht den 5er eingegeben'
-
-        String userString;
-
-        if (userInputZahl == 5) {
-            userString = "user hat den 5er eingegeben";
-        } else {
-            userString = "user hat nicht den 5er eingegeben";
-        }
-
-        System.out.println(userString);
-
-        // tenärer Operator
-        String userStringShort = userInputZahl == 5 ? "user hat größer 5 eingegeben" : "user hat kleiner 5 eingegeben";
-
-
-        // tenärer Operator - verschachtelt!
-        userStringShort =
-            userInputZahl != 5 ?
-                (userInputZahl > 5 ?
-                        "user hat größer 5 eingegeben" :
-                        "user hat kleiner 5 eingegeben"
-                ) :
-                "user hat den 5er eingegeben";
-
-        System.out.println(userStringShort);
+        // TODO: implement
     }
 }
