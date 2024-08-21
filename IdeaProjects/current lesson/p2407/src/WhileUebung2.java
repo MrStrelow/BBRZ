882c7f50468a6364c706e2d5a3542762863f4435
MrStrelow <mathias.cammerlander@gmail.com>
2024-08-21 16:13:26 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/WhileUebung2.java
+++ IdeaProjects/current lesson/p2407/src/WhileUebung2.java
@@ -2,21 +2,48 @@ import java.util.Scanner;
 
 public class WhileUebung2 {
     public static void main(String[] args) {
-        // 1.) Schreibe ein While Schleife, welche von 1 bis 10 zaehl
+//        // 1.) Schreibe ein While Schleife, welche von 1 bis 10 zaehl
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("BIS wann soll gezählt werden? ");
+//        Integer obereGrenze = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.print("AB wann soll gezählt werden? ");
+//        Integer untereGrenze = Integer.parseInt(scanner.nextLine());
+//        Integer zaehler = 1;
+//
+//        while ( (zaehler + untereGrenze - 1) <= obereGrenze) {
+//
+//            // wenn zahl gerade dann gib "gerade" aus.
+//            if (zaehler % 2 == 0) {
+//                System.out.println((zaehler + untereGrenze - 1) + " ist eine gerade Zahl und die Schleife wurde " + zaehler + " mal ausgeführt");
+//
+//                // wenn zahl ungerade dann gib "ungerade" aus.
+//            } else {
+//                System.out.println( (zaehler + untereGrenze - 1) + " ist eine ungerade Zahl und die Schleife wurde " + zaehler + " mal ausgeführt");
+//            }
+//
+//            zaehler++;
+//        }
+
         Scanner scanner = new Scanner(System.in);
 
-        Integer zaehler = 1;
-        Integer obereGrenze = ;
+        System.out.print("BIS wann soll gezählt werden? ");
+        Integer obereGrenze = Integer.parseInt(scanner.nextLine());
 
-        while (zaehler <= obereGrenze) {
+        System.out.print("AB wann soll gezählt werden? ");
+        Integer untereGrenze = Integer.parseInt(scanner.nextLine());
+        Integer zaehler = untereGrenze;
+
+        while ( zaehler  <= obereGrenze) {
 
             // wenn zahl gerade dann gib "gerade" aus.
             if (zaehler % 2 == 0) {
-                System.out.println(zaehler + " ist eine gerade Zahl.");
+                System.out.println(zaehler + " ist eine gerade Zahl und die Schleife wurde " + zaehler + " mal ausgeführt");
 
                 // wenn zahl ungerade dann gib "ungerade" aus.
             } else {
-                System.out.println(zaehler + " ist eine ungerade Zahl.");
+                System.out.println( zaehler + " ist eine ungerade Zahl und die Schleife wurde " + zaehler + " mal ausgeführt");
             }
 
             zaehler++;
