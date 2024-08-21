30cd6e90b1b9c6f2b8eee68bcdf2b2d950f248ab
MrStrelow <mathias.cammerlander@gmail.com>
2024-08-21 11:45:45 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/BeispielFlugpreise.java
+++ IdeaProjects/current lesson/p2407/src/BeispielFlugpreise.java
@@ -20,16 +20,26 @@ public class BeispielFlugpreise {
         Double basispreis = entfernung * 0.02;
 
         // 2. Info aus der Tabelle - Klasse - Zeile 2 bis 4
-        if (klasse.equals("economy")) {
-
-        } else if (klasse.equals("premium")) {
-
-        } else if (klasse.equals("erste")) {
-
-        } else {
-            System.out.println("Fehler.");
+        switch (klasse) {
+            case "premium" -> basispreis += 200;
+            case "erste" -> basispreis += 400;
+            default -> System.out.println("Fehler.");
         }
 
         // 3. Info aus der Tabelle - Monat - Zeile 5 bis 6
+        switch (monat) {
+            case 7,8,9 -> basispreis += 20;
+            case 12 -> basispreis += 15;
+        }
+
+//        if (7 <= monat && monat <= 9) {
+//            basispreis += 20;
+//
+//        } else if(monat == 12) {
+//            basispreis += 15;
+//        }
+
+        // Ausgabe
+        System.out.println(basispreis);
     }
 }
