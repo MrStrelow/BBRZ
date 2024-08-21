11514c2092a687abed9efa866382c470629f7014
MrStrelow <mathias.cammerlander@gmail.com>
2024-08-21 11:04:18 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/BeispielFlugpreise.java
+++ IdeaProjects/current lesson/p2407/src/BeispielFlugpreise.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class BeispielFlugpreise {
     public static void main(String[] args) {
+        // 1. userinput - wir lesen hier die benötigten informationen ein.
+        Scanner myScanner = new Scanner(System.in);
 
+        System.out.print("Entfernung: ");
+        Double entfernung = Double.parseDouble(myScanner.nextLine());
+
+        System.out.print("Reisemonat: ");
+
+        System.out.print("Buchungsklasse: ");
+
+        // 2. preise berechnen
     }
 }
