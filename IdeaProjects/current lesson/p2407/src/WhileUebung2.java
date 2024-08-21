d90455596fdf1755119b09d71acd259e27d3cfa4
MrStrelow <mathias.cammerlander@gmail.com>
2024-08-21 16:02:07 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/WhileUebung2.java
+++ IdeaProjects/current lesson/p2407/src/WhileUebung2.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class WhileUebung2 {
+    public static void main(String[] args) {
+        // 1.) Schreibe ein While Schleife, welche von 1 bis 10 zaehl
+        Scanner scanner = new Scanner(System.in);
+
+        Integer zaehler = 1;
+        Integer obereGrenze = ;
+
+        while (zaehler <= obereGrenze) {
+
+            // wenn zahl gerade dann gib "gerade" aus.
+            if (zaehler % 2 == 0) {
+                System.out.println(zaehler + " ist eine gerade Zahl.");
+
+                // wenn zahl ungerade dann gib "ungerade" aus.
+            } else {
+                System.out.println(zaehler + " ist eine ungerade Zahl.");
+            }
+
+            zaehler++;
+        }
+
+//        while (true) {
+//            System.out.println(zaehler);
+//
+//            if (zaehler == 10) {
+//                break;
+//            }
+//
+//            zaehler++;
+//        }
+
+    }
 }
