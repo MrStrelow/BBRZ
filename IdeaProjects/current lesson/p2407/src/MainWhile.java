02eca8d51bdbb112dccb1765b67d987e47880c58
MrStrelow <mathias.cammerlander@gmail.com>
2024-08-21 13:59:46 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/MainWhile.java
+++ IdeaProjects/current lesson/p2407/src/MainWhile.java
@@ -7,7 +7,7 @@ public class MainWhile {
         Scanner scanner = new Scanner(System.in);
 
         Random random = new Random();
-        Integer toBeGuessed = random.nextInt(0,100);
+        Integer toBeGuessed = 77; // random.nextInt(0,100);
         Integer versuche = 1; // Das ist eine Zählvariable
 
         System.out.print("Wie oft sollst du maximal raten können?: ");
@@ -15,11 +15,17 @@ public class MainWhile {
 
         while (true) {
 
+            // habe ich genug Versuche?
+            if (versuche > maximaleVersuche) {
+                System.out.println("Verloren! Keine Versuche mehr vorhanden.");
+                break;
+            }
+
             System.out.print("Gib eine Zahl ein: ");
             Integer guess = Integer.parseInt(scanner.nextLine());
 
             if (guess == toBeGuessed) {
-                System.out.println("Die Zahl wurde erraten! Sie haben " + versuche + " Versuche gebraucht.");
+                System.out.println("Gewonnen! Die Zahl wurde erraten! Sie haben " + versuche + " Versuche gebraucht.");
                 break;
 
             } else if (guess > toBeGuessed) {
