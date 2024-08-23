165b8325865b2b3bbadc0a812078296a1c8c05ba
MrStrelow <mathias.cammerlander@gmail.com>
2024-08-23 14:27:17 +0200

    sadf

--- IdeaProjects/current lesson/FreitagNachmittag/src/Main.java
+++ IdeaProjects/current lesson/FreitagNachmittag/src/Main.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static void main(String[] args) {
+        //hier gehts los
+        // <Typ> <Name> <Zuweisungsoperator> <alles was einen Wert erzeugt der den gleichen Typ hat wie ganz links>;
+
+        Scanner myScanner = new Scanner(System.in);
+        String meinText = "System.out.println();";
+        Integer a = 5;
+
+        String deinText = meinText;
+        String nochEinText = a.toString();
+
+
+        System.out.println();
+    }
 }
