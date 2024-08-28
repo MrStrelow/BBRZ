6dafdd70adf3c8ae4a777a09b318fa4ee9619c3b
MrStrelow <mathias.cammerlander@gmail.com>
2024-08-28 10:48:16 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/WhileUebung3.java
+++ IdeaProjects/current lesson/p2407/src/WhileUebung3.java
@@ -1,5 +1,23 @@
 public class WhileUebung3 {
     public static void main(String[] args) {
-        
+//        Schreiben Sie eine While-Schleife, die von 10 bis 1 alle Zahlen im Format "10-9-8-7-6-5-4-3-2-1"
+//        ausgibt.
+//        - Speichern Sie hierbei die Zahl 10 in der Konstante *bound*
+//        - Ändern Sie nun bound auf 100
+
+        // ohne format
+        Integer zaehlvariable = 10;
+
+        while (zaehlvariable >= 1) {
+            if (zaehlvariable == 1) {
+                System.out.print(zaehlvariable);
+            } else {
+
+            }
+            System.out.print(zaehlvariable + "-");
+
+            zaehlvariable--;
+        }
+
     }
 }
