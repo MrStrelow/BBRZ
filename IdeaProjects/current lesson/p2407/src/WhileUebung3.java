5c252ed574138981df00db75a032f092d074339d
MrStrelow <mathias.cammerlander@gmail.com>
2024-08-28 11:46:51 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/WhileUebung3.java
+++ IdeaProjects/current lesson/p2407/src/WhileUebung3.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.SequencedCollection;
+
 public class WhileUebung3 {
     public static void main(String[] args) {
 //        Schreiben Sie eine While-Schleife, die von 10 bis 1 alle Zahlen im Format "10-9-8-7-6-5-4-3-2-1"
@@ -39,11 +42,31 @@ public class WhileUebung3 {
             zaehlvariable++;
         }
 
+        System.out.println();
 
 
 //        Schreiben Sie ein Programm, das eine Zahl vom Benutzer einliest und dann die Summe aller Zahlen
 //        von 1 bis zur eingegebenen Zahl ausgibt
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bitte gib Zahl ein: ");
+        Integer userInput = Integer.parseInt(scanner.nextLine());
+
+        // 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1
+
+        // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+        zaehlvariable = 1;
+        Integer summe = 0;
+
+        while (zaehlvariable <= userInput) {
+            summe += zaehlvariable;
+
+//            System.out.println(summe + " : " + zaehlvariable);
+
+            zaehlvariable++;
+        }
+
+        System.out.println("Die summe von 1 bis " + userInput + " ist: " + summe);
 
 
 //        Schreiben Sie ein Programm, das den Benutzer nach einer Zahl fragt und dann die Fakultät dieser
