444d786e22314084e237c4444194d8ea2fa55938
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-04 10:12:13 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/GuessTheWord.java
+++ IdeaProjects/current lesson/p2407/src/GuessTheWord.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class GuessTheWord {
     public static void main(String[] args) {
         // - User interaktion
-        // - Eingabe des users, welche, falls diese falsch ist, wiederholt werden muss.
-        // - Wiederholtes abfragen des Rateversuches des Users.
-        // - aufende Ausgabe des Ratewortes
+        // - [x] Eingabe des users, welche, falls diese falsch ist, wiederholt werden muss.
+        // - [] Wiederholtes abfragen des Rateversuches des Users.
+        // - laufende Ausgabe des Ratewortes
 
         // Variablen
         Scanner scanner = new Scanner(System.in);
@@ -13,13 +13,37 @@ public class GuessTheWord {
         Integer maximaleSpielzüge = 6;
 
         Integer verwendeteSpielzüge = 0;
-        String wortZuErraten = "";
+        String wortZuErraten;
         Character eingabeZumRaten;
+        Boolean wortErraten = false;
+
+        String anzeigeWort = "_".repeat(laengeDesWortes);
 
         // - Eingabe des users, welche, falls diese falsch ist, wiederholt werden muss.
-        while (wortZuErraten.length() != laengeDesWortes) {
+        do {
             System.out.println("Bitte gib ein Wort mit " + laengeDesWortes + " Buchstaben ein, welches erraten werden muss.");
             wortZuErraten = scanner.nextLine();
+
+            if (wortZuErraten.length() != laengeDesWortes) {
+                System.out.println("Unpassende Länge! [" + wortZuErraten.length() + "]. Bitte Wort mit " + laengeDesWortes + " Buchstaben eingeben.");
+            }
+        } while (wortZuErraten.length() != laengeDesWortes);
+
+        // Anzeige vom Ratezustand
+        System.out.println("Beginne das Wort zu erraten!");
+        System.out.println(anzeigeWort);
+
+        // - Wiederholtes abfragen des Rateversuches des Users.
+        while (verwendeteSpielzüge < maximaleSpielzüge || wortErraten ) {
+            // 0 | 0 | Ergebnis oder: 0 | Ergebnis und: 0
+            // 0 | 1 | Ergebnis oder: 1 | Ergebnis und: 0
+            // 1 | 0 | Ergebnis oder: 1 | Ergebnis und: 0
+
+            // 1 | 1 | Ergebnis oder: 1 | Ergebnis und: 1
+
+            System.out.println(verwendeteSpielzüge);
+
+            verwendeteSpielzüge++;
         }
 
     }
