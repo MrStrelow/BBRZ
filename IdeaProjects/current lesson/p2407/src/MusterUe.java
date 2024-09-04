bd0d8cb0b3a25f867cdcdb71109b8a34786abdaf
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-04 15:50:50 +0200

    asdf

---
+++ IdeaProjects/current lesson/p2407/src/MusterUe.java
@@ -0,0 +1,46 @@
+public class MusterUe {
+    public static void main(String[] args) {
+//        Aufgabe 1:
+//        Gib folgendes Muster mithilfe von Schleifen in der Konsole aus.
+//        #
+//        ##
+//        ###
+//        ####
+//        #####
+//        ######
+
+        Integer limit = 6;
+
+        for (int i = 0; i < limit; i++) {
+            for (int j = 1; j <= 2*i; j++) {
+                System.out.print("#");
+                //  + i + " " + j
+            }
+
+            if (i == 0) {
+                System.out.print("#");
+            }
+
+            System.out.println();
+        }
+
+
+//        Aufgabe 2
+//        Gib folgendes Muster mithilfe von Schleifen in der Konsole aus.
+//        #
+//        ##
+//        ###
+//        ####
+//        #####
+//        ######
+//        #####
+//        ####
+//        ###
+//        ##
+//        #
+
+        for (int i = 0; i < limit; i++) {
+            System.out.println("#".repeat(i));
+        }
+    }
+}
