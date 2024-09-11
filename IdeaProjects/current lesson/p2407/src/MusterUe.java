b1c7bd9aae2d4488328afba423f2c21bba0d3d5a
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-11 09:47:26 +0200

    asdf

--- IdeaProjects/current lesson/p2407/src/MusterUe.java
+++ IdeaProjects/current lesson/p2407/src/MusterUe.java
@@ -1,5 +1,20 @@
 public class MusterUe {
     public static void main(String[] args) {
+
+        Integer zaehlvariable = 0; // i
+
+        while (zaehlvariable < 6) {
+            System.out.println(zaehlvariable);
+
+            zaehlvariable++;
+        }
+
+        // 3 Teile
+
+        for (Integer i = 0; i < 6; i++) {
+            System.out.println(i);
+        }
+
 //        Aufgabe 1:
 //        Gib folgendes Muster mithilfe von Schleifen in der Konsole aus.
 //        #
@@ -11,22 +26,24 @@ public class MusterUe {
 
         Integer limit = 6;
 
-        for (int i = 1; i <= limit; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int zeilen = 1; zeilen <= limit; zeilen++) {
+            for (int spalten = 1; spalten <= zeilen; spalten++) {
                 System.out.print("#");
-                //  + i + " " + j
+                //  + zeilen + " " + spalten
             }
             System.out.println();
         }
 
+        System.out.println();
+
 //        Aufgabe 1.5:
 //        Gib folgendes Muster mithilfe von Schleifen in der Konsole aus.
 //        #
+//        ##
 //        ####
 //        ######
 //        ########
 //        ##########
-//        ############
 
         for (int i = 0; i < limit; i++) {
             for (int j = 1; j <= 2*i; j++) {
@@ -41,6 +58,7 @@ public class MusterUe {
             System.out.println();
         }
 
+        System.out.println();
 
 //        Aufgabe 2
 //        Gib folgendes Muster mithilfe von Schleifen in der Konsole aus.
@@ -56,12 +74,23 @@ public class MusterUe {
 //        ##
 //        #
 
-        for (int i = 0; i < limit; i++) {
-            System.out.println("#".repeat(i));
+        for (int zeilen = 1; zeilen <= limit; zeilen++) {
+            for (int spalten = 1; spalten <= zeilen; spalten++) {
+                System.out.print("#");
+                //  + zeilen + " " + spalten
+            }
+            System.out.println();
         }
 
-        for (int i = limit; i >= 0; i--) {
-            System.out.println("#".repeat(i));
-        }
+
+
+
+//        for (int i = 0; i < limit; i++) {
+//            System.out.println("#".repeat(i));
+//        }
+//
+//        for (int i = limit; i >= 0; i--) {
+//            System.out.println("#".repeat(i));
+//        }
     }
 }
