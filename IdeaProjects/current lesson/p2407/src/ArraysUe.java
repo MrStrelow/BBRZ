5a316284db33ddbe87b7621b192c29d3a5b94af0
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-11 13:23:23 +0200

    asdf

---
+++ IdeaProjects/current lesson/p2407/src/ArraysUe.java
@@ -0,0 +1,10 @@
+public class ArraysUe {
+    public static void main(String[] args) {
+        String[] myStringArray = {"asdf", "qwer"};
+
+        String myString = myStringArray[0];
+
+        System.out.println(myStringArray[1]);
+        System.out.println(myString);
+    }
+}
