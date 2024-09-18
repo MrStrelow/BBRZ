a537fbe45119b5796ee7c03d94aa78a2603e079b
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-18 16:06:36 +0200

    asdf

--- BITEC/current lesson/p2407/src/UePalindrom.java
+++ BITEC/current lesson/p2407/src/UePalindrom.java
@@ -1,17 +1,63 @@
 public class UePalindrom {
     public static void main(String[] args) {
-        String s = "anna";
+        String s = "Anna".toLowerCase();
 
         // Ich weiß, dass ein Palindrom von vorne und hinten gelesen, das gleiche Wort ergibt.
 
         // Vartiante 1
+        System.out.println("########## Variante 1 ##########");
         String reversS = new StringBuilder(s).reverse().toString();
         System.out.println( s.equals(reversS) );
 
+        System.out.println("########## Variante 2 ##########");
         // Variante 2
         Integer links = 0;
         Integer rechts = s.length()-1;
 
-        System.out.println( s. );
+        Boolean istPalindrom = false;
+
+        while (s.charAt(links) == s.charAt(rechts)) {
+            links++;
+            rechts--;
+
+            if (links > rechts) {
+                istPalindrom = true;
+                break;
+            }
+        }
+
+        System.out.println(istPalindrom);
+
+         System.out.println("########## Variante 3 ##########");
+        // Variante 3
+        istPalindrom = true;
+
+        while (true) {
+            links++;
+            rechts--;
+
+            if (s.charAt(links) != s.charAt(rechts)) {
+                istPalindrom = false;
+                break;
+            } else {
+                istPalindrom = true;
+            }
+        }
+
+        System.out.println("########## Variante 4 ##########");
+        // Variante 4
+        istPalindrom = false;
+
+        while (s.charAt(links) == s.charAt(rechts)) {
+            links++;
+            rechts--;
+
+            if (s.length()/2 >= links) {
+                istPalindrom = true;
+                break;
+            }
+        }
+
+        System.out.println(istPalindrom);
     }
 }
