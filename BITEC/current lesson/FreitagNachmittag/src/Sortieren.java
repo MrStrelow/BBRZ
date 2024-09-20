d120dc81fe9496646e3b26dd077fc59985c09605
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-20 14:58:08 +0200

    asdfasdf

---
+++ BITEC/current lesson/FreitagNachmittag/src/Sortieren.java
@@ -0,0 +1,12 @@
+public class Sortieren {
+    public static void main(String[] args) {
+        // 1. Block: Vergleiche die zwei ersten Zahlen eines Arrays miteinander.
+        // Wenn die erste großer als die zweite ist, dann vertausche diese. Ansonsten nicht.
+
+        // lege ein array mit zahlen 28, 6, 24, 4, 12
+
+        //frage ab ob die zahl bei position 0 großer als jene bei position 1 ist.
+        // wenn ja, vertausche
+        // wenn nein tu nichts.
+    }
+}
