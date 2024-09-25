0c8945cef927d0f3d72f57d0c26e76bbe9e648de
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-25 13:05:55 +0200

    asdfasdf

--- BITEC/current lesson/p2407/src/ArraysPhrasomat.java
+++ BITEC/current lesson/p2407/src/ArraysPhrasomat.java
@@ -1,5 +1,9 @@
 public class ArraysPhrasomat {
     public static void main(String[] args) {
-
+// Pools von Wörtern je Wortgruppe (Nomen und Verben)
+        String[] nomen = {"Dog", "Tree", "Cat", "Fish", "Orange", "Gauda Cheese", "Scarf", "Laptop", "JAVA", "Mouse", "BBRZ", "Christmas"};
+        String[] verb = {"empowers", "adores", "carries", "heats", "froze", "ate", "blames", "injures"};
+        String[] adjektiv = {"yellow", "big", "small", "cheeky", "dark", "bright", "dim", "sad"};
+        String[] sensationen = {"unglaubliche", "spektakulaere", "unfassbare", "oage"};
     }
 }
