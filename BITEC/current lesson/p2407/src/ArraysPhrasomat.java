ce2c0640a37232c101d07e307fc9525e36216c74
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-25 14:03:00 +0200

    asdfasdf

--- BITEC/current lesson/p2407/src/ArraysPhrasomat.java
+++ BITEC/current lesson/p2407/src/ArraysPhrasomat.java
@@ -6,37 +6,54 @@ public class ArraysPhrasomat {
         // Pools von Wörtern je Wortgruppe (Nomen und Verben)
         String[] nomen = {"Dog", "Tree", "Cat", "Fish", "Orange", "Gauda Cheese", "Scarf", "Laptop", "JAVA", "Mouse", "BBRZ", "Christmas"};
         String[] verb = {"empowers", "adores", "carries", "heats", "froze", "ate", "blames", "injures"};
-//        String[] adjektiv = {"yellow", "big", "small", "cheeky", "dark", "bright", "dim", "sad"};
+        String[] adjektiv = {"yellow", "big", "small", "cheeky", "dark", "bright", "dim", "sad"};
 //        String[] sensationen = {"unglaubliche", "spektakulaere", "unfassbare", "oage"};
 
-        // 1.
-        // b)
+        // 1 b)
+        // Objekte
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        // SchleifenBedingungen
         Boolean unzufriedenPhrase = true;
 
-        while (unzufriedenPhrase) {
-            // a)
-            Integer randomNumberFirstNomen = random.nextInt(0, nomen.length);
-            Integer randomNumberVerb = random.nextInt(0, verb.length);
-            Integer randomNumberSecondNomen = random.nextInt(0, nomen.length);
+        // Variablen
+        String phrase = ""; // TODO: nur zum testen, damit wir nicht alle 7 fälle des switches und
+        // default ausprogrammieren müssen bevor wir das programm ausführen könne
 
-            String phrase = nomen[randomNumberFirstNomen] + " " + verb[randomNumberVerb] + " " + nomen[randomNumberSecondNomen];
+        // 2)
+        System.out.println("Wie lange soll die Phrase werden? [3-7]: ");
+        Integer anzahlWoerter = Integer.parseInt(scanner.nextLine());
+
+        while (unzufriedenPhrase) {
+            // 1 a)
+            Integer randomNumberFirstNomen = random.nextInt(0, nomen.length);
+            Integer randomNumberSecondNomen = random.nextInt(0, nomen.length);
+            Integer randomNumberVerb = random.nextInt(0, verb.length);
+            Integer randomNumberAdjective = random.nextInt(0, adjektiv.length);
+
+            switch (anzahlWoerter) {
+                case 3 -> {
+                    phrase = nomen[randomNumberFirstNomen] + " " + verb[randomNumberVerb] + " " + nomen[randomNumberSecondNomen];
+                }
+                case 4 -> {
+                    if (random.nextDouble() < 0.5) {
+                        phrase = adjektiv[randomNumberAdjective] + " " + nomen[randomNumberFirstNomen] + " " + verb[randomNumberVerb] + " " + nomen[randomNumberSecondNomen];
+                    } else {
+                        phrase = nomen[randomNumberFirstNomen] + " " + verb[randomNumberVerb] + " " + adjektiv[randomNumberAdjective] + " " + nomen[randomNumberSecondNomen];
+                    }
+                }
+                case 5 -> {
+
+                }
+                case 6 -> {}
+                case 7 -> {}
+                default -> {}
+            }
             System.out.println(phrase);
 
             System.out.print("Sind Sie zufrieden mit der Phrase? [ja/nein]: ");
             String meinung = scanner.nextLine();
-
-//            if (meinung.equals("ja")) {
-//                unzufriedenPhrase = false;
-//
-//            } else if (meinung.equals("nein")) {
-//                unzufriedenPhrase = true;
-//
-//            } else {
-//                System.out.println("Bitte geben Sie ja oder nein ein");
-//                unzufriedenPhrase = true;
-//            }
 
             switch (meinung) {
                 case "ja" -> unzufriedenPhrase = false;
