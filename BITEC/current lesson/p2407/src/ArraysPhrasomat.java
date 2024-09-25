5b7b7ed99a3b229b4335d62b0133f154467ae6c4
MrStrelow <mathias.cammerlander@gmail.com>
2024-09-25 14:16:23 +0200

    asdfasdf

--- BITEC/current lesson/p2407/src/ArraysPhrasomat.java
+++ BITEC/current lesson/p2407/src/ArraysPhrasomat.java
@@ -2,6 +2,17 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ArraysPhrasomat {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public static void main(String[] args) {
         // Pools von Wörtern je Wortgruppe (Nomen und Verben)
         String[] nomen = {"Dog", "Tree", "Cat", "Fish", "Orange", "Gauda Cheese", "Scarf", "Laptop", "JAVA", "Mouse", "BBRZ", "Christmas"};
@@ -22,7 +33,7 @@ public class ArraysPhrasomat {
         // default ausprogrammieren müssen bevor wir das programm ausführen könne
 
         // 2)
-        System.out.println("Wie lange soll die Phrase werden? [3-7]: ");
+        System.out.print("Wie lange soll die Phrase werden? [3-7]: ");
         Integer anzahlWoerter = Integer.parseInt(scanner.nextLine());
 
         while (unzufriedenPhrase) {
@@ -34,13 +45,24 @@ public class ArraysPhrasomat {
 
             switch (anzahlWoerter) {
                 case 3 -> {
-                    phrase = nomen[randomNumberFirstNomen] + " " + verb[randomNumberVerb] + " " + nomen[randomNumberSecondNomen];
+                    phrase =
+                             ANSI_BLUE + nomen[randomNumberFirstNomen] + " " +
+                             ANSI_RED  + verb[randomNumberVerb]        + " " +
+                             ANSI_BLUE + nomen[randomNumberSecondNomen];
                 }
                 case 4 -> {
                     if (random.nextDouble() < 0.5) {
-                        phrase = adjektiv[randomNumberAdjective] + " " + nomen[randomNumberFirstNomen] + " " + verb[randomNumberVerb] + " " + nomen[randomNumberSecondNomen];
+                        phrase =
+                                 ANSI_GREEN + adjektiv[randomNumberAdjective] + " " +
+                                 ANSI_BLUE  + nomen[randomNumberFirstNomen]   + " " +
+                                 ANSI_RED   + verb[randomNumberVerb]          + " " +
+                                 ANSI_BLUE  + nomen[randomNumberSecondNomen];
                     } else {
-                        phrase = nomen[randomNumberFirstNomen] + " " + verb[randomNumberVerb] + " " + adjektiv[randomNumberAdjective] + " " + nomen[randomNumberSecondNomen];
+                        phrase =
+                                ANSI_BLUE  + nomen[randomNumberFirstNomen]   + " " +
+                                ANSI_RED   + verb[randomNumberVerb]          + " " +
+                                ANSI_GREEN + adjektiv[randomNumberAdjective] + " " +
+                                ANSI_BLUE  + nomen[randomNumberSecondNomen];
                     }
                 }
                 case 5 -> {
@@ -50,9 +72,10 @@ public class ArraysPhrasomat {
                 case 7 -> {}
                 default -> {}
             }
-            System.out.println(phrase);
 
-            System.out.print("Sind Sie zufrieden mit der Phrase? [ja/nein]: ");
+            System.out.println("Die generierte Phrase ist: " + phrase);
+
+            System.out.print(ANSI_RESET + "Sind Sie zufrieden mit der Phrase? [ja/nein]: ");
             String meinung = scanner.nextLine();
 
             switch (meinung) {
@@ -64,8 +87,6 @@ public class ArraysPhrasomat {
                 }
             }
 
-            System.out.println("##########################################");
-            System.out.println("Die generierte Phrase ist: " + phrase);
         }
 
 
