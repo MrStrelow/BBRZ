f492a9076e18428bbf7de3db3193a605cf607937
MrStrelow <mathias.cammerlander@gmail.com>
2024-10-16 11:27:52 +0200

    asdfasdf

--- BITEC/current lesson/p2407/src/FunktionenBeispielMuster.java
+++ BITEC/current lesson/p2407/src/FunktionenBeispielMuster.java
@@ -2,27 +2,55 @@ public class FunktionenBeispielMuster {
     public static void main(String[] args) {
         String[][] feld = fillCanvas(new String[6][6], "~");
         String[][] dreieck = drawTriangle(feld, "#");
-        String[][] diamant = drawDiamant(dreieck);
+        String[][] diamant = drawDiamond(dreieck);
         print(diamant);
     }
 
     // Achtung! static immer vor die Funktion schreiben. Brauchen wir hier wegen der Objektorientierung. Das lernen wir später kennen.
     // funktion welche ein 2d Array auf die Console ausgibt
-    static void print(String[][] feld) {
+    static void print(String[][] field) {
 
     }
 
     // funktion welche in ein 2d Array eine Raute(Diamant) zeichnet
+    static String[][] drawDiamond(String[][] triangle) {
+        String[][] topRight = triangle;
+        String[][] botRight = mirrorX(triangle);
+        String[][] botLeft = mirrorY(botRight);
+        String[][] topLeft = mirrorX(botLeft);
+
+        String[][] diamond = new String[][];
+        combineForm(diamond, topRight, )
+        return null;
+    }
 
     // funktion welche in ein 2d Array ein Dreieck zeichnet
+    static String[][] drawTriangle(String[][] field, String symbol) {
+        return null;
+    }
 
     // funktion welche ein 2d Array mit einem Symbol befüllt
+    static String[][] fillCanvas(String[][] field, String symbol) {
+        return null;
+    }
 
     // funktion um alles zusammenzufügen - diese funktion wird in der drawDiamant aufgerufen
+    static String[][] combineForm(String[][] diamond, String[][] triangle, Position position) {
+        return null;
+    }
 
     // funktion welche muster um x achse spiegelt - diese funktion wird in der drawDiamant aufgerufen
+    static String[][] mirrorX(String[][] field) {
+        return null;
+    }
 
     // funktion welche muster um y achse spiegelt - diese funktion wird in der drawDiamant aufgerufen
+    static String[][] mirrorY(String[][] field) {
+        return null;
+    }
 
+}
 
+enum Position {
+    TOPRIGHT, TOPLEFT, BOTRIGHT, BOTLEFT
 }
