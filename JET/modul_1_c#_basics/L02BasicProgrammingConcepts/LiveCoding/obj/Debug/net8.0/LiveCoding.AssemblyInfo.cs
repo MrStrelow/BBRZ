753f3e56c5660e09c260a5364dec85788bba5e31
MrStrelow <mathias.cammerlander@gmail.com>
2024-10-18 08:02:34 +0200

    asdfsadf

--- JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/obj/Debug/net8.0/LiveCoding.AssemblyInfo.cs
+++ JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/obj/Debug/net8.0/LiveCoding.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ecae7190e74c313b378cdd302b2cd4b137328ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653e82a2dee8715173351a0bd0896b93dc96ac13")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
