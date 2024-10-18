d9ad188ad99887a2b7686b7c63fb3894749b6bca
MrStrelow <mathias.cammerlander@gmail.com>
2024-10-18 11:11:16 +0200

    asdfsadf

--- JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/Program.cs
+++ JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/Program.cs
@@ -9,7 +9,7 @@
         int size = 6;
         string whiteSquareCode = "\u2588";
         string blackSquareCode = '\u2591'.ToString();
-        string[,] brett = new string[size, size];
+        string[,] board = new string[size, size];
 
 
         //for (int y = 0; y < brett.GetLength(0); y++)
@@ -42,31 +42,46 @@
         //}
 
 
-        for (int y = 0; y < brett.GetLength(0); y++)
+        for (int y = 0; y < board.GetLength(0); y++)
         {
-            for (int x = 0; x < brett.GetLength(1); x++)
+            for (int x = 0; x < board.GetLength(1); x++)
             {
                 //if ( (x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1) )
                 if ((x + y) % 2 == 0)
                 {
-                    brett[y, x] = blackSquareCode;
+                    board[y, x] = blackSquareCode;
                 }
                 else
                 {
-                    brett[y, x] = whiteSquareCode;
+                    board[y, x] = whiteSquareCode;
                 }
             }
         }
 
+        Console.WriteLine("Bitte Start Koordinaten angeben [y x]: ");
+        string[] userinput = Console.ReadLine().Split();
+        int yStart = int.Parse(userinput[0]);
+        int xStart;
+        int yEnd;
+        int xEnd;
+
+        board[0, 0] = "o";
+        board[5, 3] = "x";
+        double k = ;
 
         // TODO: here linie coden
-
-
-        for (int y = 0; y < brett.GetLength(1); y++)
+        for (int i = 1; i < 5; i++)
         {
-            for (int x = 0; x < brett.GetLength(0); x++)
+            int j = (int) (k * i);
+            board[i, j] = "~";
+        }
+
+
+        for (int y = 0; y < board.GetLength(1); y++)
+        {
+            for (int x = 0; x < board.GetLength(0); x++)
             {
-                Console.Write(brett[y, x]);
+                Console.Write(board[y, x]);
             }
             Console.WriteLine();
         }
