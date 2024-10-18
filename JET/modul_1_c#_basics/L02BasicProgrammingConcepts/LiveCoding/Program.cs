f7f41820911e78cee261790836def614d9f79642
MrStrelow <mathias.cammerlander@gmail.com>
2024-10-18 08:22:50 +0200

    sdfasdf

--- JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/Program.cs
+++ JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/Program.cs
@@ -6,40 +6,57 @@
         // 1.) userinput
 
         // 2.) schachbrett erstellen
-        int size = 5;
+        int size = 6;
         string whiteSquareCode = "\u2588";
         string blackSquareCode = '\u2591'.ToString();
         string[,] brett = new string[size, size];
 
 
-        for (int y = 0; y < brett.GetLength(1); y++)
+        //for (int y = 0; y < brett.GetLength(0); y++)
+        //{
+        //    for (int x = 0; x < brett.GetLength(1); x++)
+        //    {
+        //        if (x % 2 == 0)
+        //        {
+        //            if (y % 2 == 0)
+        //            {
+        //                brett[y, x] = blackSquareCode;
+        //            }
+        //            else
+        //            {
+        //                brett[y, x] = whiteSquareCode;
+        //            }
+        //        } 
+        //        else
+        //        {
+        //            if (y % 2 == 0)
+        //            {
+        //                brett[y, x] = whiteSquareCode;
+        //            }
+        //            else
+        //            {
+        //                brett[y, x] = blackSquareCode;
+        //            }
+        //        }
+        //    }
+        //}
+
+
+        for (int y = 0; y < brett.GetLength(0); y++)
         {
-            for (int x = 0; x < brett.GetLength(0); x++)
+            for (int x = 0; x < brett.GetLength(1); x++)
             {
-                if (x % 2 == 0)
+                if (???)
                 {
-                    if (y % 2 == 0)
-                    {
-                        brett[y, x] = blackSquareCode;
-                    }
-                    else
-                    {
-                        brett[y, x] = whiteSquareCode;
-                    }
-                } 
+                    brett[y, x] = blackSquareCode;
+                }
                 else
                 {
-                    if (y % 2 == 0)
-                    {
-                        brett[y, x] = whiteSquareCode;
-                    }
-                    else
-                    {
-                        brett[y, x] = blackSquareCode;
-                    }
+                    brett[y, x] = whiteSquareCode;
                 }
             }
         }
+
 
         for (int y = 0; y < brett.GetLength(1); y++)
         {
