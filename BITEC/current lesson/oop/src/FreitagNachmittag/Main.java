adbd2b97cebde2b0b5bda22aa91cc8b7cbed1c30
MrStrelow <mathias.cammerlander@gmail.com>
2024-11-22 13:51:22 +0100

    asdf

--- BITEC/current lesson/oop/src/FreitagNachmittag/Main.java
+++ BITEC/current lesson/oop/src/FreitagNachmittag/Main.java
@@ -3,8 +3,5 @@ package FreitagNachmittag;
 public class Main {
     public static void main(String[] args) {
         Spielfeld spielfeld = new Spielfeld(5);
-
-        Hamster hempter = new Hamster(null, 0, 0);
-        System.out.println(hempter.getDarstellung());
     }
 }
