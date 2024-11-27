5e16ce2c78428a5a075f7bd9f50e55c8341462b4
MrStrelow <mathias.cammerlander@gmail.com>
2024-11-27 11:53:09 +0100

    asdf

--- BITEC/lessons/block_2_object_orientation/src/lerneinheiten/L05CollectionsUndDictionaries/mylist/LinkedList.java
+++ BITEC/lessons/block_2_object_orientation/src/lerneinheiten/L05CollectionsUndDictionaries/mylist/LinkedList.java
@@ -1,4 +1,17 @@
 package lerneinheiten.L05CollectionsUndDictionaries.mylist;
 
 public class LinkedList {
+    // Felder
+    // ??
+
+    // Hat-Beziehungen
+    private Node head;
+
+    // Konstruktor
+    // ??
+
+    // Methoden
+    public void add() {
+
+    }
 }
