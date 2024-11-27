56d2de4b9d1f817d5964a78c7bc887e492f76c15
MrStrelow <mathias.cammerlander@gmail.com>
2024-11-27 11:50:27 +0100

    asdf

--- BITEC/lessons/block_2_object_orientation/src/lerneinheiten/L05CollectionsUndDictionaries/mylist/Main.java
+++ BITEC/lessons/block_2_object_orientation/src/lerneinheiten/L05CollectionsUndDictionaries/mylist/Main.java
@@ -2,6 +2,18 @@ package lerneinheiten.L05CollectionsUndDictionaries.mylist;
 
 public class Main {
     public static void main(String[] args) {
+        // Erstelle Nodes (Objekte) welche auch dessen Wert setzten.
+        Node firstNode = new Node(3);
+        Node secondNode = new Node(4);
+        Node thirdNode = new Node(5);
 
+        // Setze die Nachbarn!
+        firstNode.setNachbar(secondNode);
+        secondNode.setNachbar(thirdNode);
+//        thirdNode.setNachbar(firstNode);
+
+        // lösche second node aus der Liste
+        firstNode.setNachbar(thirdNode);
+        secondNode = null;
     }
 }
