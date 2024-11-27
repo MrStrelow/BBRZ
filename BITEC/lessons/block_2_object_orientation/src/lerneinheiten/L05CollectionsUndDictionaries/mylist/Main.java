cc59c513ef0373d48da626598565db86f5fa7155
MrStrelow <mathias.cammerlander@gmail.com>
2024-11-27 14:26:31 +0100

    asdf

--- BITEC/lessons/block_2_object_orientation/src/lerneinheiten/L05CollectionsUndDictionaries/mylist/Main.java
+++ BITEC/lessons/block_2_object_orientation/src/lerneinheiten/L05CollectionsUndDictionaries/mylist/Main.java
@@ -22,6 +22,9 @@ public class Main {
         myList.add(3);
         myList.add(4);
         myList.add(5);
+        myList.add(6);
+        myList.add(7);
+        myList.add(8);
         myList.display();
 
         myList.remove(1);
