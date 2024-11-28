0d404ff87796185241d7c5954b46c244a48240f0
MrStrelow <mathias.cammerlander@gmail.com>
2024-11-28 12:15:27 +0100

    sadfafs

--- JET/modul_1_c#_basics/L01MyFirstDotNetProject/livecoding/Program.cs
+++ JET/modul_1_c#_basics/L01MyFirstDotNetProject/livecoding/Program.cs
@@ -9,19 +9,24 @@
 
 // oder einfach.... ohne dem hier...
 
-int[] numbers = [5,5,3,1,8,7,2,4];
-
-// (1.) Implementieren das Vertauschen des 1. und 2. Elements (mit index 0 und 1, ohne Schleife)
-if (numbers[0] > numbers[1])
-{
-    int temp = numbers[0];
-    numbers[0] = numbers[1];
-    numbers[1] = temp;
-}
+int[] numbers = [6,5,3,1,8,7,2,4];
 
 // (2.) Implementiere das Vertauschen des i und i+1 elements, für eine "Bubble".
+for ( int i = 0; i < numbers.Length - 1; i++ )
+{
+    // (1.) Implementieren das Vertauschen des 1. und 2. Elements (mit index 0 und 1, ohne Schleife)
+    if (numbers[i] > numbers[i+1])
+    {
+        int temp = numbers[i];
+        numbers[i] = numbers[i+1];
+        numbers[i+1] = temp;
+    }
+
+    Console.WriteLine($"{i + 1} Paar [{string.Join(",", numbers)}]");
+}
+
+
 
 // (3.) Implementiere das Wiederholen von (2.) für alle "Bubbles" 
 
 
-Console.WriteLine($"{0 + 1} Paar [{string.Join(",", numbers)}]");
