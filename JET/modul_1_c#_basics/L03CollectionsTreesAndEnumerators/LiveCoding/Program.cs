7370195d95c2fb6796c65c0c6c163a7063d93fbc
MrStrelow <mathias.cammerlander@gmail.com>
2024-11-29 09:08:37 +0100

    asdf

--- JET/modul_1_c#_basics/L03CollectionsTreesAndEnumerators/LiveCoding/Program.cs
+++ JET/modul_1_c#_basics/L03CollectionsTreesAndEnumerators/LiveCoding/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿class Program
+{
+    static string blue = "\u001B[34m";
+    static string red = "\u001B[31m";
+    static string reset = "\u001B[0m";
+
+    static void Main(string[] args)
+    {
+        // ProduktTypen
+        string fahrrad = "Fahrrad";
+        string tisch = "Tisch";
+        string kulli = "Kulli";
+
+        // Boxen
+        var bigBox = new List<string>();
+        var anotherBigBox = new List<string>();
+        var mediumBox = new List<string>();
+        var smallBox = new List<string>();
+
+    }
+}
