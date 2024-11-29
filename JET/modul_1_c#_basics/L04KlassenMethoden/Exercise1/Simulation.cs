91ebc351e9ddc2248245bf48562b61c9e3649408
MrStrelow <mathias.cammerlander@gmail.com>
2024-11-29 08:44:09 +0100

    Fix JAVA Style Classes

---
+++ JET/modul_1_c#_basics/L04KlassenMethoden/Exercise1/Simulation.cs
@@ -0,0 +1,26 @@
+﻿using Hamster;
+using System.Text;
+
+namespace Hamster;
+
+public class Simulation
+{
+    static void Main(String[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Plane meinFeld = new Plane();
+
+        while (true)
+        {
+            foreach (Hamster hamster in meinFeld.getHamsters())
+            {
+                hamster.Metabolize();
+                hamster.Move();
+            }
+
+            meinFeld.PrintPlane();
+
+            Thread.Sleep(500);
+        }
+    }
+}
