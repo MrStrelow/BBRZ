207fd665a62f9fd6bce31e68ba6d3169d5836775
MrStrelow <mathias.cammerlander@gmail.com>
2024-12-04 13:20:55 +0100

    asdf

---
+++ BITEC/current lesson/oop/src/AbstractClassesUndInterfaces/schnittstellen/interfaces/Strukturierbar.java
@@ -0,0 +1,7 @@
+package AbstractClassesUndInterfaces.schnittstellen.interfaces;
+
+import AbstractClassesUndInterfaces.schnittstellen.impl.Form;
+
+public interface Strukturierbar {
+    Form generieForm();
+}
