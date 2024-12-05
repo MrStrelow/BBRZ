f7809ec88f25d3e40b08f5b6dffba17968d08218
MrStrelow <mathias.cammerlander@gmail.com>
2024-12-05 11:23:44 +0100

    asdf

--- JET/modul_1_c#_basics/L02BasicProgrammingConcepts/L02BasicProgrammingConcepts/obj/Debug/net8.0/L02BasicProgrammingConcepts.AssemblyInfo.cs
+++ JET/modul_1_c#_basics/L02BasicProgrammingConcepts/L02BasicProgrammingConcepts/obj/Debug/net8.0/L02BasicProgrammingConcepts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("L02BasicProgrammingConcepts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0eb4d3892a72e46180a9de447b995094473dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24fff0da7505abfac36528ea7d2e3789428c2ef9")]
 [assembly: System.Reflection.AssemblyProductAttribute("L02BasicProgrammingConcepts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("L02BasicProgrammingConcepts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/obj/Debug/net8.0/LiveCoding.AssemblyInfo.cs
+++ JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/obj/Debug/net8.0/LiveCoding.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0eb4d3892a72e46180a9de447b995094473dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24fff0da7505abfac36528ea7d2e3789428c2ef9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
