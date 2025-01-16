9919a1c25f28b1d76bfc7b55054063a62fc542d4
MrStrelow <mathias.cammerlander@gmail.com>
2025-01-16 09:51:27 +0100

    asdf

--- JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/AP10Formen.cs
+++ JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/AP10Formen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ class AP10Formen
         string[,] field = fillCanvas(new string[3, 3]);
         print(field);
 
-        mirrorX(mirrorY(field));
+        mirrorX(field);
     }
 
     static string[,] fillCanvas(string[,] field)
@@ -46,12 +47,21 @@ class AP10Formen
     static string[,] mirrorX(string[,] field)
     {
         // 1.) besuche jedes Feld im Array (wie wird das in der Methode print gemacht?)
-        // 2.) schiebe jedes Feld auf einen neuen Ort (field[y,x] = field[y,length-1-y]). 
+        for (int y = 0; y < field.GetLength(0); y++)
+        {
+            for (int x = 0; x < field.GetLength(1); x++) 
+            {
+                // 2.) schiebe jedes Feld auf einen neuen Ort (field[length-1-y,x] = field[y,x]). 
+                field[field.GetLength(0) - 1 - y, x] = field[y, x];
+            }
+        }
+
         return field;
     }
 
     static string[,] mirrorY(string[,] field)
     {
+        (field[y, x] = field[y, length - 1 - y]).
         return null;
     }
 }
