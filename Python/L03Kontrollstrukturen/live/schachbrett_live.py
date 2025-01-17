f4ed7fd62f2b6e79d5d07d42f6ab221f48e2a5fc
MrStrelow <mathias.cammerlander@gmail.com>
2025-01-17 14:21:54 +0100

    asdf

--- Python/L03Kontrollstrukturen/live/schachbrett_live.py
+++ Python/L03Kontrollstrukturen/live/schachbrett_live.py
@@ -1,11 +1,14 @@
 # 1.) userinput: 
 #   - die größe des spielbretts soll angegeben werden.
-#   - welche position soll als start gewählt werden, welche als ziel.
+#   - TODO: welche position soll als start gewählt werden, welche als ziel.
 
 # i.) user informieren was er/sie eingeben soll
 dimension = int(input("Größe des Spielbretts eingeben: "))
 # print(type(dimension))
-print(dimension)
 
 # 2.) schachbrett generieren
+
+print(dimension)
+
+
 # 3.) linien auf den schachbrett von start zu ende zeichnen (achtung ein wenig mathe)
