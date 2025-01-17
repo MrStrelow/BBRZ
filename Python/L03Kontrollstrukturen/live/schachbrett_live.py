5915e127733a3cc404bc204635fc06fe5140a428
MrStrelow <mathias.cammerlander@gmail.com>
2025-01-17 15:55:54 +0100

    asdf

--- Python/L03Kontrollstrukturen/live/schachbrett_live.py
+++ Python/L03Kontrollstrukturen/live/schachbrett_live.py
@@ -5,6 +5,8 @@
 # i.) user informieren was er/sie eingeben soll
 dimension = int(input("Größe des Spielbretts eingeben: "))
 # print(type(dimension))
+x_start, y_start = input("Startpunkt wählen [x y]: ").split(" ")
+x_end, y_end = input("Endpunkt wählen [x y]: ").split(" ")
 
 # 2.) schachbrett generieren
 black_square = "⬛" # windows + . (oder "\u2B1B")
@@ -14,14 +16,17 @@ board = []
 for y in range(dimension): # erzeugt mir [0,1,2,3,4] wenn user 5 eingibt
     row = []
     
-    for x in range(dimension): 
-        row.append(black_square)
-        #board.append(white_square)
+    for x in range(dimension):
+        if (x + y) % 2 == 0: # (x % 2 == 0 and y % 2 == 0) or (x % 2 == 1 and y % 2 == 1):
+            row.append(white_square)
+        else:
+            row.append(black_square)
+
 
     board.append(row)  
 
-
 # 3.) linien auf den schachbrett von start zu ende zeichnen (achtung ein wenig mathe)
+
 
 # 4.) ausgabe des schachbretts
 for y in range(dimension):
