92595fea07baa389d1ecc1ec9bc5fec73ad7ac23
MrStrelow <mathias.cammerlander@gmail.com>
2025-01-30 13:32:37 +0100

    asdf

--- JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/Formen.cs
+++ JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/Formen.cs
@@ -1,142 +1,142 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection.Metadata;
+//using System.Runtime.InteropServices;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace LiveCoding;
+//namespace LiveCoding;
 
-class AP10Formen
-{
-    static void Main(string[] args)
-    {
-        string[,] triangle = DrawTriangle(FillCanvas(new string[9,9]));
-        string[,] container = FillCanvas(new string[2 * triangle.GetLength(0), 2 * triangle.GetLength(1)]);
+//class AP10Formen
+//{
+//    static void Main(string[] args)
+//    {
+//        string[,] triangle = DrawTriangle(FillCanvas(new string[9,9]));
+//        string[,] container = FillCanvas(new string[2 * triangle.GetLength(0), 2 * triangle.GetLength(1)]);
 
-        Print(CombineForm(container, triangle, Position.TOP_RIGHT));
-        Console.WriteLine();
+//        Print(CombineForm(container, triangle, Position.TOP_RIGHT));
+//        Console.WriteLine();
 
-        Print(CombineForm(container, MirrorY(triangle), Position.TOP_LEFT));
-        Console.WriteLine();
+//        Print(CombineForm(container, MirrorY(triangle), Position.TOP_LEFT));
+//        Console.WriteLine();
 
-        Print(CombineForm(container, MirrorX(MirrorY(triangle)), Position.BOT_LEFT));
-        Console.WriteLine();
+//        Print(CombineForm(container, MirrorX(MirrorY(triangle)), Position.BOT_LEFT));
+//        Console.WriteLine();
 
-        Print(CombineForm(container, MirrorX(triangle), Position.BOT_RIGHT));
+//        Print(CombineForm(container, MirrorX(triangle), Position.BOT_RIGHT));
         
-        // Warum geht das?... denke an call by value und call by reference.
-        //Print(CombineForm(container, MirrorY(triangle), Position.TOP_LEFT));
-    }
+//        // Warum geht das?... denke an call by value und call by reference.
+//        //Print(CombineForm(container, MirrorY(triangle), Position.TOP_LEFT));
+//    }
 
-    static string[,] FillCanvas(string[,] field)
-    {
-        for (int y = 0; y < field.GetLength(0); y++)
-        {
-            for (int x = 0; x < field.GetLength(1); x++)
-            {
-                field[y, x] = "_";
-            }
-        }
+//    static string[,] FillCanvas(string[,] field)
+//    {
+//        for (int y = 0; y < field.GetLength(0); y++)
+//        {
+//            for (int x = 0; x < field.GetLength(1); x++)
+//            {
+//                field[y, x] = "_";
+//            }
+//        }
 
-        return field;
-    }
+//        return field;
+//    }
 
-    static void Print(string[,] field)
-    {
-        for (int y = 0; y < field.GetLength(0); y++)
-        {
-            for (int x = 0; x < field.GetLength(1); x++)
-            {
-                Console.Write(field[y, x]);
-            }
+//    static void Print(string[,] field)
+//    {
+//        for (int y = 0; y < field.GetLength(0); y++)
+//        {
+//            for (int x = 0; x < field.GetLength(1); x++)
+//            {
+//                Console.Write(field[y, x]);
+//            }
 
-            Console.WriteLine();
-        }
-    }
+//            Console.WriteLine();
+//        }
+//    }
 
 
-    static string[,] DrawTriangle(string[,] field)
-    {
-        // 1.) zeichne 
-        // ___
-        // ___
-        // ___
-        //
-        // #__
-        // ##_
-        // ###
-        // in field rein.
+//    static string[,] DrawTriangle(string[,] field)
+//    {
+//        // 1.) zeichne 
+//        // ___
+//        // ___
+//        // ___
+//        //
+//        // #__
+//        // ##_
+//        // ###
+//        // in field rein.
 
-        for (int y = 0; y < field.GetLength(0); y++)
-        {
-            for (int x = 0; x < field.GetLength(1); x++)
-            {
-                if (x <= y)
-                {
-                    field[y, x] = "#";
-                }
-            }
-        }
+//        for (int y = 0; y < field.GetLength(0); y++)
+//        {
+//            for (int x = 0; x < field.GetLength(1); x++)
+//            {
+//                if (x <= y)
+//                {
+//                    field[y, x] = "#";
+//                }
+//            }
+//        }
 
-        return field;
+//        return field;
 
-    }
+//    }
 
-    static string[,] MirrorX(string[,] field)
-    {
-        string[,] ret = new string[field.GetLength(0), field.GetLength(1)];
-        // 1.) besuche jedes Feld im Array (wie wird das in der Methode print gemacht?)
-        for (int y = 0; y < field.GetLength(0); y++)
-        {
-            for (int x = 0; x < field.GetLength(1); x++) 
-            {
-                // 2.) schiebe jedes Feld auf einen neuen Ort (field[length-1-y,x] = field[y,x]). 
-                ret[field.GetLength(0) - 1 - y, x] = field[y, x];
-            }
-        }
+//    static string[,] MirrorX(string[,] field)
+//    {
+//        string[,] ret = new string[field.GetLength(0), field.GetLength(1)];
+//        // 1.) besuche jedes Feld im Array (wie wird das in der Methode print gemacht?)
+//        for (int y = 0; y < field.GetLength(0); y++)
+//        {
+//            for (int x = 0; x < field.GetLength(1); x++) 
+//            {
+//                // 2.) schiebe jedes Feld auf einen neuen Ort (field[length-1-y,x] = field[y,x]). 
+//                ret[field.GetLength(0) - 1 - y, x] = field[y, x];
+//            }
+//        }
 
-        return ret;
-    }
+//        return ret;
+//    }
 
-    static string[,] MirrorY(string[,] field)
-    {
-        string[,] ret = new string[field.GetLength(0), field.GetLength(1)];
-        // 1.) besuche jedes Feld im Array (wie wird das in der Methode print gemacht?)
-        for (int y = 0; y < field.GetLength(0); y++)
-        {
-            for (int x = 0; x < field.GetLength(1); x++)
-            {
-                // 2.) schiebe jedes Feld auf einen neuen Ort (field[y,length-1-x] = field[y,x]). 
-                ret[y, field.GetLength(1) - 1 - x] = field[y, x];
-            }
-        }
+//    static string[,] MirrorY(string[,] field)
+//    {
+//        string[,] ret = new string[field.GetLength(0), field.GetLength(1)];
+//        // 1.) besuche jedes Feld im Array (wie wird das in der Methode print gemacht?)
+//        for (int y = 0; y < field.GetLength(0); y++)
+//        {
+//            for (int x = 0; x < field.GetLength(1); x++)
+//            {
+//                // 2.) schiebe jedes Feld auf einen neuen Ort (field[y,length-1-x] = field[y,x]). 
+//                ret[y, field.GetLength(1) - 1 - x] = field[y, x];
+//            }
+//        }
 
-        return ret;
-    }
+//        return ret;
+//    }
 
-    static string[,] CombineForm(string[,] container, string[,] part, Position position)
-    {
-        for (int y = 0; y < part.GetLength(0); y++)
-        {
-            for (int x = 0; x < part.GetLength(1); x++)
-            {
-                switch(position)
-                {
-                    case Position.TOP_RIGHT: container[y, x + part.GetLength(1)] = part[y, x]; break;
-                    case Position.TOP_LEFT:  container[y, x] = part[y, x]; break;
-                    case Position.BOT_RIGHT: container[y + part.GetLength(0), x + part.GetLength(1)] = part[y, x]; break;
-                    case Position.BOT_LEFT:  container[y + part.GetLength(0), x] = part[y, x]; break;
-                }
-            }
-        }
+//    static string[,] CombineForm(string[,] container, string[,] part, Position position)
+//    {
+//        for (int y = 0; y < part.GetLength(0); y++)
+//        {
+//            for (int x = 0; x < part.GetLength(1); x++)
+//            {
+//                switch(position)
+//                {
+//                    case Position.TOP_RIGHT: container[y, x + part.GetLength(1)] = part[y, x]; break;
+//                    case Position.TOP_LEFT:  container[y, x] = part[y, x]; break;
+//                    case Position.BOT_RIGHT: container[y + part.GetLength(0), x + part.GetLength(1)] = part[y, x]; break;
+//                    case Position.BOT_LEFT:  container[y + part.GetLength(0), x] = part[y, x]; break;
+//                }
+//            }
+//        }
 
-        return container;
-    }
-}
+//        return container;
+//    }
+//}
 
-enum Position
-{
-    TOP_RIGHT, TOP_LEFT, BOT_RIGHT, BOT_LEFT
-}
+//enum Position
+//{
+//    TOP_RIGHT, TOP_LEFT, BOT_RIGHT, BOT_LEFT
+//}
--- JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/obj/Debug/net8.0/LiveCoding.AssemblyInfo.cs
+++ JET/modul_1_c#_basics/L02BasicProgrammingConcepts/LiveCoding/obj/Debug/net8.0/LiveCoding.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee008380c3e02ee9f3dad816b65a710de2da5b44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d446cf8469a914753612b9c81e9a8fae86702685")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiveCoding")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
