57b6c01371189f84b7d72e86b4209a8fbc806837
MrStrelow <mathias.cammerlander@gmail.com>
2025-02-12 15:25:58 +0100

    asdf

--- BITEC/current lesson/imperativ/src/Test.java
+++ BITEC/current lesson/imperativ/src/Test.java
@@ -14,7 +14,8 @@ public class Test {
         Integer meineZahl = 15;
         Double meinFloat = 5.5;
         String meinString = "hallo";
-        Boolean meineWahrheit = true;
+        Boolean A = true;
+        Boolean B = true;
         System.out.println(meineZahl); //Snippet: sout
 
         // Deklaration einer Variable.
@@ -23,6 +24,34 @@ public class Test {
         System.out.println(eineZahl);
 
         // Operatoren verbinden variablen
-        meineWahrheit &&
+        boolean a = A && A;
+        System.out.println(a);
+
+        a = (!A || B) && A; // 1.
+//        a = (!true || true) && true; // 2.
+//        a = (false || true) && true; // 3.
+//        a = true && true; // 3.
+//        a = true; // 3.
+        System.out.println(a);
+
+        a = !A || (A && A);
+
+        System.out.println(5 / 3);
+        System.out.println(5 % 3);
+        System.out.println(5. / 3);
+
+        String[][] feld = new String[5][5];
+
+        for (int zeile = 0; zeile < feld.length; zeile++) {
+            for (int spalte = 0; spalte < feld.length; spalte++) {
+//                if ( (zeile % 2 == 0 && spalte % 2 == 0) || (zeile % 2 == 1 && spalte % 2 == 1) ) {
+                if ( (zeile + spalte) % 2 == 0 ) {
+                    System.out.print("🟦");
+                } else {
+                    System.out.print("⬜");
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
