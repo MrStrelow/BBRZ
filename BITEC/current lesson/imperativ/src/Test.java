10b01ed504145a6eee35cd3668a9af1e01ebac9d
MrStrelow <mathias.cammerlander@gmail.com>
2025-02-12 13:20:09 +0100

    asdf

--- BITEC/current lesson/imperativ/src/Test.java
+++ BITEC/current lesson/imperativ/src/Test.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
 public class Test {
+    int hallod = 5;
+
     public static void main(String[] args) { // Snippet: main
         System.out.println("hallo"); // Wert: String
         System.out.println(6); // Wert: Integer
 
         // <Typ> <Name> <Zuweisungsoperator> <Wert>;
+        // Definition einer Variable.
         int meineZahl = 5;
-        System.out.println(); //Snippet: sout
+        System.out.println(meineZahl); //Snippet: sout
     }
 }
