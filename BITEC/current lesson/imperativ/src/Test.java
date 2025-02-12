5e463a254ea155633707eb1e948b298ff7d291d5
MrStrelow <mathias.cammerlander@gmail.com>
2025-02-12 12:15:12 +0100

    asdf

--- BITEC/current lesson/imperativ/src/Test.java
+++ BITEC/current lesson/imperativ/src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("hallo"); // Wert
+    public static void main(String[] args) { // Snippet: main
+        System.out.println("hallo"); // Wert: String
+        System.out.println(6); // Wert: Integer
+
+        // <Typ> <Name> <Zuweisungsoperator> <Wert>;
+        int meineZahl = 5;
+        System.out.println(); //Snippet: sout
     }
 }
