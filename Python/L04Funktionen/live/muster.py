5e242d5eae1bcf40154ce571772697d2cbe61d5e
MrStrelow <mathias.cammerlander@gmail.com>
2025-02-14 15:28:26 +0100

    asfd

--- Python/L04Funktionen/live/muster.py
+++ Python/L04Funktionen/live/muster.py
@@ -1,7 +1,14 @@
-dimension = 10
+dimension = 3
+
+# Enum
+class Position:
+    TOP_RIGHT = 1
+    TOP_LEFT = 2
+    BOT_RIGHT = 3
+    BOT_LEFT = 4
 
 # Funktionen:
-def fill_canvas(field, symbol):
+def fill_canvas(field, symbol="⬜"):
     for y in range(dimension):
         for x in range(dimension):
             field[y][x] = symbol
@@ -24,31 +31,71 @@ def draw_triangle(field, symbol):
 
 
 def mirror_x(field):
+    copy_of_field = copy(field)
+
     for y in range(len(field)):
         for x in range(len(field)):
-            field[y][x] = field[len(field) - 1 - y][x]
+            copy_of_field[y][x] = field[len(field) - 1 - y][x]
+
+    return copy_of_field
+
 
 def mirror_y(field):
-    return field
+    copy_of_field = copy(field)
+
+    for y in range(len(field)):
+        for x in range(len(field)):
+            copy_of_field[y][x] = field[y][len(field) - 1 - x]
+
+    return copy_of_field
+
+
+def copy(field):
+    # copy_of_field = []
+    # for y in range(dimension):
+    #     row = []
+    #     for x in range(dimension):
+    #         row.append(field[y][x])
+
+    #     copy_of_field.append(row)
+
+    return [[elem for elem in row] for row in field]
+    # return [row[:] for row in field]
+    # return [row for row in field]
+
+
+def zammstoepsln():
+    return
+
+
+def draw_diamant(triangle_top_right):
+    triangle_top_left  = mirror_y(triangle_top_right)
+    triangle_bot_right = mirror_x(triangle_top_right)
+    triangle_bot_left  = mirror_x(triangle_top_left)
+
+    container = [["" for _ in range(dimension * 2)] for _ in range(dimension * 2)]
+    diamant = zammstoepsln(container, triangle_top_right, Position.TOP_RIGHT)
+    diamant = zammstoepsln(container, triangle_top_left, Position.TOP_LEFT)
+    diamant = zammstoepsln(container, triangle_bot_right, Position.BOT_RIGHT)
+    diamant = zammstoepsln(container, triangle_bot_left, Position.BOT_LEFT)
+
+    return diamant
+
 
 # Verwende Funktionen:
-field = []
-for _ in range(dimension):
-    row = []
-    for _ in range(dimension):
-        row.append("_")
-    field.append(row)
+# field = []
+# for _ in range(dimension):
+#     row = []
+#     for _ in range(dimension):
+#         row.append("_")
+#     field.append(row)
 
-# field = [["_" for _ in range(dimension)] for _ in range(dimension)]
+field = [["" for _ in range(dimension)] for _ in range(dimension)]
 
-field = fill_canvas(field, symbol="◽")
+field = fill_canvas(
+    symbol = "◽", 
+    field  = field
+)
 
-top_right = draw_triangle(field, symbol="🔷")
-top_left  = mirror_y(top_right)
-bot_right = mirror_x(top_right)
-bot_left  = mirror_x(top_left)
-
-print_canvas(top_right)
-# print_canvas(top_left)
-# print_canvas(bot_right)
-# print_canvas(bot_left)
+triangle = draw_triangle(field, symbol="🔷")
+draw_diamant(triangle)
