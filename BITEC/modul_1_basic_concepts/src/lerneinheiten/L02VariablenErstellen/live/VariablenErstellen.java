9ad78da404ce7dc82fdb69701a16c43ec740be58
MrStrelow <mathias.cammerlander@gmail.com>
2025-02-26 10:10:05 +0100

    asfd

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L02VariablenErstellen/live/VariablenErstellen.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L02VariablenErstellen/live/VariablenErstellen.java
@@ -1,4 +1,23 @@
 package lerneinheiten.L02VariablenErstellen.live;
 
 public class VariablenErstellen {
+    public static void main(String[] args) {
+        // Startpunkt
+        // <Typ> <Name>
+        // definiert
+        String definiert;
+
+        // initialisiert
+        // <Name> <Zuweisungsoperator> <Wert>
+        definiert = "hallo";
+
+        int primitiv = 3;
+
+        // <Typ> <Namen> <Zuweisungsoperator> <Wert>
+        Integer wrapper = primitiv;
+
+        String initialisertUndDefiniert = wrapper.toString();
+
+        System.out.println(definiert);
+    }
 }
