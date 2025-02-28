46bdb53497e37163f5adfc357e0cf13016c38d6d
MrStrelow <mathias.cammerlander@gmail.com>
2025-02-28 14:00:45 +0100

    asffs

--- Python/L04Funktionen/live/3_decorator.py
+++ Python/L04Funktionen/live/3_decorator.py
@@ -16,9 +16,15 @@ def say_hello(eins, zwei):
     print(f"{eins}, Hallo! {zwei}")
 
 
+@print_davor_und_danach(True)
+def say_bye():
+    print("Bye!")
+
+
 ####################### Main #######################
 # decorator = print_davor_und_danach(True)
 # wrapper = decorator(say_hello)
 # wrapper("Du, ", "Wie gehts?")
 
 say_hello("Du, ", "Wie gehts?")
+say_bye()
