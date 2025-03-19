ed5584bd5663568d846db50125b9cfe18514a1de
MrStrelow <mathias.cammerlander@gmail.com>
2025-03-19 10:32:52 +0100

    asfd

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L05VariablenUmwandeln/live/SchachbrettKoordinaten.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L05VariablenUmwandeln/live/SchachbrettKoordinaten.java
@@ -1,5 +1,8 @@
 package lerneinheiten.L05VariablenUmwandeln.live;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class SchachbrettKoordinaten {
@@ -7,17 +10,30 @@ public class SchachbrettKoordinaten {
         // 1. nextLine
         Scanner scanner = new Scanner(System.in);
 
+        // Startpunkt einlesen
         System.out.print("Wähle die Spielfigur - x y: ");
         String input = scanner.nextLine(); // lies die ganze zeile ein
         String[] teile = input.split("[ \\-,/🧱🔺\n]"); // reiß diesen string an den angegebenen symbolen auseinander
         Integer xStart = Integer.parseInt(teile[0]); // nimm die erste zahl
         Integer yStart = Integer.parseInt(teile[1]); // nimm die 2. zahl
 
-        //----
-
+        // Endpunkt einlesen
         System.out.print("Wähle das Ziel - x y: ");
         input = scanner.nextLine();
+        teile = input.split("[ \\-,/🧱🔺\n]"); // reiß diesen string an den angegebenen symbolen auseinander
+        Integer xEnd = Integer.parseInt(teile[0]); // nimm die erste zahl
+        Integer yEnd = Integer.parseInt(teile[1]); // nimm die 2. zahl
 
-        //----
+        // distanz berechnen
+        Double distanz = Math.sqrt(Math.pow(xEnd - xStart,2) + Math.pow(yEnd - yStart,2));
+
+        // formatiere distanz auf , mit 2 nachkommastellen
+        DecimalFormat decimalFormat = new DecimalFormat();
+        decimalFormat.applyPattern("#.##"); // 2 nachkommastellen
+        decimalFormat.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.GERMAN)); //wir zwingen egal welche sprache am computer ist, einen beistrich zu machen.
+
+        String formatedDistanz = decimalFormat.format(distanz);
+        System.out.println(formatedDistanz);
+
     }
 }
--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L05VariablenUmwandeln/uebung/Loesung.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L05VariablenUmwandeln/uebung/Loesung.java
@@ -7,58 +7,59 @@ import java.util.Scanner;
 
 public class Loesung {
     public static void main(String[] args) {
-        // 1. nextInt
+
+        // 1. mit nextLine - kommentiere 1. aus mit Ctrl+/ auf dem numpad oder /* dazwischen ist alles ein Kommentar */
         Scanner scanner = new Scanner(System.in);
-        scanner.useDelimiter("[ \\-,/🧱🔺\n]");
+        String regex = "[ \\-,/🧱🔺\n]";
 
         System.out.print("Wähle die Spielfigur [x y]: ");
-        Integer xStart = scanner.nextInt();
-        Integer yStart = scanner.nextInt();
+        String input = scanner.nextLine();
+
+        String[] teile = input.split(regex);
+        Integer xStart = Integer.parseInt(teile[0]);
+        Integer yStart = Integer.parseInt(teile[1]);
 
         System.out.print("Wähle die das Ziel [x y]: ");
-        Integer xEnd = scanner.nextInt();
-        Integer yEnd = scanner.nextInt();
+        input = scanner.nextLine(); // Achtung! wir überschreiben eine bestehende Variable.
+
+        teile = input.split(regex); // Achtung! wir überschreiben eine bestehende Variable.
+        Integer xEnd = Integer.parseInt(teile[0]);
+        Integer yEnd = Integer.parseInt(teile[1]);
 
         Double distanz = Math.sqrt(Math.pow(xEnd - xStart, 2) + Math.pow(yEnd - yStart, 2));
-
         DecimalFormat df = new DecimalFormat();
         df.applyPattern("#.##");
         df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.GERMANY));
 
         String formattedDistanz = df.format(distanz);
 
+        System.out.println("Die Distanz beträgt: " + df.format(distanz));
         System.out.println("Die Figur auf Position [x:" + xStart + " y:" + yStart + "] wurde auf Position [x:" + xEnd +" y:" + yEnd + "] geschoben. Distanz: " + formattedDistanz);
 
         System.out.print("Wie geht es dir? ");
         String wieGehts = scanner.nextLine();
         System.out.println("Mir gehts " + wieGehts + ".");
 
-//        // 2. mit nextLine - kommentiere 1. aus mit Ctrl+/ auf dem numpad oder /* dazwischen ist alles ein Kommentar */
+//        // 2. nextInt
 //        Scanner scanner = new Scanner(System.in);
-//        String regex = "[ \\-,/🧱🔺\n]";
+//        scanner.useDelimiter("[ \\-,/🧱🔺\n]");
 //
 //        System.out.print("Wähle die Spielfigur [x y]: ");
-//        String input = scanner.nextLine();
-//
-//        String[] teile = input.split(regex);
-//        Integer xStart = Integer.parseInt(teile[0]);
-//        Integer yStart = Integer.parseInt(teile[1]);
+//        Integer xStart = scanner.nextInt();
+//        Integer yStart = scanner.nextInt();
 //
 //        System.out.print("Wähle die das Ziel [x y]: ");
-//        input = scanner.nextLine(); // Achtung! wir überschreiben eine bestehende Variable.
-//
-//        teile = input.split(regex); // Achtung! wir überschreiben eine bestehende Variable.
-//        Integer xEnd = Integer.parseInt(teile[0]);
-//        Integer yEnd = Integer.parseInt(teile[1]);
+//        Integer xEnd = scanner.nextInt();
+//        Integer yEnd = scanner.nextInt();
 //
 //        Double distanz = Math.sqrt(Math.pow(xEnd - xStart, 2) + Math.pow(yEnd - yStart, 2));
+//
 //        DecimalFormat df = new DecimalFormat();
 //        df.applyPattern("#.##");
 //        df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.GERMANY));
 //
 //        String formattedDistanz = df.format(distanz);
 //
-//        System.out.println("Die Distanz beträgt: " + df.format(distanz));
 //        System.out.println("Die Figur auf Position [x:" + xStart + " y:" + yStart + "] wurde auf Position [x:" + xEnd +" y:" + yEnd + "] geschoben. Distanz: " + formattedDistanz);
 //
 //        System.out.print("Wie geht es dir? ");
