cdc5234bdf0861fcf5162fd1a87f1369579f2c75
MrStrelow <mathias.cammerlander@gmail.com>
2025-03-26 13:24:39 +0100

    asdf

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L06VerzweigungenUndBedingungenMitIF/live/BuchstabenErkennen.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L06VerzweigungenUndBedingungenMitIF/live/BuchstabenErkennen.java
@@ -1,4 +1,23 @@
 package lerneinheiten.L06VerzweigungenUndBedingungenMitIF.live;
 
+import java.util.Scanner;
+
 public class BuchstabenErkennen {
+    public static void main(String[] args) {
+//        Vokal?
+//        Schreiben Sie ein Java-Programm, das den Benutzer nach einem Buchstaben fragt und überprüft, ob es
+//        sich um einen Vokal oder einen Konsonanten handelt. Wenn es ein Vokal ist, gibt das Programm "Das ist ein
+//        Vokal" aus, ansonsten "Das ist ein Konsonant".
+
+        // frage ob userput vokal ist
+        // WENN ja, DANN sag ist vokal
+        // ANSONSTEN sag ist kein vokal
+
+        // Variablen: scanner, eingabeDesUsers, variablen für Bedingungen
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Buchstaben eingeben: ");
+        String userinput = scanner.nextLine();
+
+    }
 }
