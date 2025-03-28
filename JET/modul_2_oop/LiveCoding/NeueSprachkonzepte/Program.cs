c9c28d8546a8eff63a001ba170227cb2c6aef1cf
MrStrelow <mathias.cammerlander@gmail.com>
2025-03-28 09:31:01 +0100

    asdf

--- JET/modul_2_oop/LiveCoding/NeueSprachkonzepte/Program.cs
+++ JET/modul_2_oop/LiveCoding/NeueSprachkonzepte/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿public class Person
+{
+    // Felder -> Eigenschaften
+    public string Name { get; set; }
+    private char nickName;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        this.age = age;
+        this.nickName = 'a';
+    }
+
+    public Person(char nickName, int age)
+    {
+        this.nickName = nickName;
+        this.age = age;
+        Name = "";
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public void SetAge(int age)
+    {
+        //if (age > 0)
+            this.age = age;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Person hans = new Person(name: "hans", age: 1);
+        Console.WriteLine(hans.Name);
+        hans.Name = "asdf";
+        Console.WriteLine(hans.Name);
+        //Person hans = new Person(nickName: "asdf", age: 1);
+        //Person hans = new Person(age: 1);
+    }
+}
---
+++ JET/modul_2_oop/LiveCoding/NeueSprachkonzepte/alt.md
@@ -0,0 +1,33 @@
+﻿```csharp
+public class Person
+{
+    private string name;
+    private char nickName;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+        this.nickName = 'a';
+    }
+
+    public Person(char nickName, int age)
+    {
+        this.nickName = nickName;
+        this.age = age;
+        this.name = "";
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public void SetAge(int age)
+    {
+        if (age > 0)
+            this.age = age;
+    }
+}
+```
