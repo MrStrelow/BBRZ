64cadad90bdbd4d6cb0274f1f79c92532c8ddb0a
MrStrelow <mathias.cammerlander@gmail.com>
2025-04-16 14:02:02 +0200

    asf

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L09SchleifenFor/live/Muster.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L09SchleifenFor/live/Muster.java
@@ -1,20 +1,68 @@
 package lerneinheiten.L09SchleifenFor.live;
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.util.Scanner;
 
 public class Muster {
     public static void main(String[] args) {
+        System.out.println("##ende außerste###");
+        System.out.println();
+
         // userinput
         Scanner scanner = new Scanner(System.in);
         System.out.print("Gib die größe des Musters ein [zahl]: ");
         Integer groesseSpielfeld = scanner.nextInt();
 
         // mustergenerierung
-        for (int zeilen = 0; zeilen < groesseSpielfeld; zeilen++) {
-//            for (int spalten = groesseSpielfeld - 1; spalten >= 0; spalten--) {
+        for (int zeilen = groesseSpielfeld - 1; zeilen >= 0; zeilen--) {
+            // links oben
             for (int spalten = 0; spalten < groesseSpielfeld; spalten++) {
 
-                if (zeilen >= spalten) {
+                if (spalten >= zeilen) {
+                    System.out.print("⬜");
+
+                } else {
+                    System.out.print("🔹");
+                }
+
+            }
+
+            // rechts oben
+            int verkehrzeZeilen = groesseSpielfeld - 1 - zeilen;
+            for (int spalten = 0; spalten < groesseSpielfeld; spalten++) {
+
+                if (verkehrzeZeilen >= spalten) {
+                    System.out.print("⬜");
+
+                } else {
+                    System.out.print("🔹");
+                }
+
+            }
+
+            System.out.println();
+        }
+
+        // mustergenerierung
+        for (int zeilen = 0; zeilen < groesseSpielfeld; zeilen++) {
+            // links unten
+            for (int spalten = 0; spalten < groesseSpielfeld; spalten++) {
+
+                if (spalten >= zeilen) {
+                    System.out.print("⬜");
+
+                } else {
+                    System.out.print("🔹");
+                }
+
+            }
+
+            // rechts unten
+            int verkehrzeZeilen = groesseSpielfeld - 1 - zeilen;
+            for (int spalten = 0; spalten < groesseSpielfeld; spalten++) {
+
+                if (verkehrzeZeilen >= spalten) {
                     System.out.print("⬜");
 
                 } else {
