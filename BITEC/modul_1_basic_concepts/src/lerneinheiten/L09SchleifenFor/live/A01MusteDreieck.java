3006767a5b782540cb86a7503e978403077c79b0
MrStrelow <mathias.cammerlander@gmail.com>
2025-04-22 19:55:30 +0200

    asdf

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L09SchleifenFor/live/A01MusteDreieck.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L09SchleifenFor/live/A01MusteDreieck.java
@@ -4,48 +4,14 @@ import java.util.Scanner;
 
 public class A01MusteDreieck {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("------Aufgabe 9------");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Höhe des Baumes (ohne Stamm): ");
-        int hoehe = scanner.nextInt();
-
-        for (int i = 1; i <= hoehe; i++) {
-            for (int j = hoehe - i; j > 0; j--) {
-                System.out.print(" ");
-            }
-
-            for (int k = 1; k <= i; k++) {
-                System.out.print("🌲");
-            }
-
-            System.out.println();
-        }
-//        end of upper part
-//        beginning of lower part
-        for (int i = 1; i <= hoehe/3; i++) {
-
-            System.out.print(" ");
-
-            for (int j = hoehe - 3; j > 0; j--) {
-                System.out.print(" ");
-            }
-
-            for (int k = 2; k > 0; k--) {
-                System.out.print("🟫");
-            }
-
-            System.out.println();
-        }
-
         // userinput
-        scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Gib die größe des Musters ein [zahl]: ");
         Integer groesseSpielfeld = scanner.nextInt();
 
         // mustergenerierung
         // Dreieck muster
-        for (int zeilen = 0; zeilen < groesseSpielfeld; zeilen = 0) {
+        for (int zeilen = 0; zeilen < groesseSpielfeld; zeilen++) {
             for (int spalten = 0; spalten < groesseSpielfeld; spalten++) {
                 if (zeilen >= spalten) {
                     System.out.print("⬜");
