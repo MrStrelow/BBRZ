70cf22ce46570da323c1b4b2b348b011188baac0
MrStrelow <mathias.cammerlander@gmail.com>
2025-04-30 10:23:35 +0200

    asdf

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L09SchleifenFor/live/C01Fibonacci.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L09SchleifenFor/live/C01Fibonacci.java
@@ -1,7 +1,34 @@
 package lerneinheiten.L09SchleifenFor.live;
 
+import java.util.Scanner;
+
 public class C01Fibonacci {
     public static void main(String[] args) {
+        // Variablen
+        Scanner scanner = new Scanner(System.in);
+        int fibCurrent = 0;
+        int fibNext = 1;
+        int ergebnis = 0;
 
+        // Userinput
+        System.out.print("Wie viele Fibonacci-Zahlen berechnen? ");
+        int grenze = scanner.nextInt();
+        System.out.print(fibCurrent + " + " + fibNext);
+
+        // Kontrollstrukturen
+        for (int i = 0; i < grenze - 1; i++) {
+            ergebnis = fibCurrent + fibNext;
+
+            fibCurrent = fibNext;
+            fibNext = ergebnis;
+
+            if (i != grenze - 2) {
+                System.out.print(" + " + fibNext);
+            }
+        }
+
+        System.out.print(" = " + ergebnis);
+
+        scanner.close();
     }
 }
