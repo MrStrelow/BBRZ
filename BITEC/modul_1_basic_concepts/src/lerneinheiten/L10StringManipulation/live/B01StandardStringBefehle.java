6badd803668719b372977ac23ee9e99528237ac8
MrStrelow <mathias.cammerlander@gmail.com>
2025-05-14 12:11:36 +0200

    asdf

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L10StringManipulation/live/B01StandardStringBefehle.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L10StringManipulation/live/B01StandardStringBefehle.java
@@ -9,25 +9,62 @@ public class B01StandardStringBefehle {
         String vergleich = "Dies ist ein Satz welcher uEberprueft wird.";
         StringBuilder aha = new StringBuilder("Dies ist ein Satz welcher ueberprueft wird.");
 
-//        System.out.println(text == vergleich);
-        System.out.println(text.equals(vergleich));
-//        System.out.println(text.toLowerCase().equals(vergleich.toLowerCase()));
-        System.out.println(text.equalsIgnoreCase(vergleich));
-        System.out.println(text.equalsIgnoreCase(vergleich));
-        System.out.println(text.contentEquals(aha));
+////        System.out.println(text == vergleich);
+//        System.out.println(text.equals(vergleich));
+////        System.out.println(text.toLowerCase().equals(vergleich.toLowerCase()));
+//        System.out.println(text.equalsIgnoreCase(vergleich));
+//        System.out.println(text.equalsIgnoreCase(vergleich));
+//        System.out.println(text.contentEquals(aha));
+//
+//        // Kommt ein Wort in einem String vor
+//        System.out.println(text.contains("ueberprueft"));
+//        System.out.println(text.startsWith("Dies i"));
+//        System.out.println(text.endsWith("ueberprueft wird."));
+//
+//        Scanner scanner = new Scanner(System.in);
+//        String lowerCaseVergleich = vergleich.toLowerCase();
+//        System.out.println("Eingabe: ");
+//        String userInput = scanner.nextLine().toLowerCase();
+//        boolean userInputKommtVor = lowerCaseVergleich.contains(userInput);
+//        System.out.println(userInputKommtVor);
+//
+//        // Wie extrahiere ich einen Teil eines Strings?
+//        // ... mit substring
+//        int berechneterStart = 50;
+//        int berechnetesEnde = 50;
+//
+//        if (berechneterStart < text.length() && berechnetesEnde < text.length()) {
+//            System.out.println(text.substring(berechneterStart, berechnetesEnde));
+//        } else {
+//            System.out.println("geht nicht");
+//        }
 
-        // Kommt ein Wort in einem String vor
-        System.out.println(text.contains("ueberprueft"));
-        System.out.println(text.startsWith("Dies i"));
-        System.out.println(text.endsWith("ueberprueft wird."));
+//        // ... mit substring
+//        System.out.println("zahlen angeben: ");
+//        int start = scanner.nextInt();
+//        int ende = scanner.nextInt();
+//
+//        berechneterStart = Math.max(Math.min(start, text.length()), 0);
+//        berechnetesEnde = Math.min(Math.max(ende, 0), text.length());
+//        System.out.println(berechneterStart);
+//        System.out.println(berechnetesEnde);
+//        System.out.println(text.substring(berechneterStart, berechnetesEnde));
 
-        Scanner scanner = new Scanner(System.in);
-        String lowerCaseVergleich = vergleich.toLowerCase();
-        String userInput = scanner.nextLine().toLowerCase();
-        boolean userInputKommtVor = lowerCaseVergleich.contains(userInput);
-        System.out.println(userInputKommtVor);
+        // ... mit charAt
+        text = "Wi🌊rd⬜🟩🟫.🐹";
+        for (int i = 0; i < text.length(); i++) {
+//            System.out.println("i:" + i + " - " + text.charAt(i));
+//            System.out.println("i:" + i + " - " + text.substring(i, i+1));
+            int unicode = text.codePointAt(i);
+            String result = Character.toString(unicode);
+            System.out.println("i:" + i + " - " + result + " - " + Integer.toHexString(unicode));
 
-        // Wie extrahiere ich einen Teil eines Strings?
+//            if (Character.isEmoji(unicode)) {
+            if (Integer.toHexString(unicode).length() > 4) {
+                i++;
+            }
+        }
 
+        System.out.println(text.indexOf("🐹"));
     }
 }
