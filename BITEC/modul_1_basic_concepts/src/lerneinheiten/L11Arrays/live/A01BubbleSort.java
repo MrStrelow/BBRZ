df38495dde4aa4aad90f82dca73d5d95862722f9
MrStrelow <mathias.cammerlander@gmail.com>
2025-05-14 15:34:00 +0200

    asdf

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L11Arrays/live/A01BubbleSort.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L11Arrays/live/A01BubbleSort.java
@@ -6,20 +6,30 @@ import java.util.Scanner;
 
 public class A01BubbleSort {
     public static void main(String[] args) {
+        // userinput
 //        int[] zahlen = {3, 5 ,9, 105, 64, 2};
         Scanner scanner = new Scanner(System.in);
         String inhaltDesArrays = scanner.nextLine();
 
         String[] userinput = inhaltDesArrays.split(" ");
-        int[] zahlen = 
+        Integer[] zahlen = new Integer[userinput.length];
 
-        System.out.println(Arrays.toString(userinput));
+//        System.out.println(Arrays.toString(userinput));
+//        System.out.println(userinput.getClass());
 
         for (int i = 0; i < userinput.length; i++) {
-            int res = Integer.parseInt(userinput[i]);
+            zahlen[i] = Integer.parseInt(userinput[i]);
         }
+
+//        System.out.println(Arrays.toString(zahlen));
+//        System.out.println(zahlen.getClass());
 
 //        int[] zahlen = new int[];
 
+
+        // sortiere userinput
+
+
+        
     }
 }
