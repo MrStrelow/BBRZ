0091ecef4749885c60a360cacca92adde6644c53
MrStrelow <mathias.cammerlander@gmail.com>
2025-05-16 15:37:32 +0200

    asdf

--- Python/L00ErsteSchritte/live/erste_schritte.py
+++ Python/L00ErsteSchritte/live/erste_schritte.py
@@ -1,21 +1,33 @@
-# import time
-a = "hallo"
-a = 3.7
-a = 1
+# # import time
+# a = "hallo"
+# a = 3.7
+# a = 1
 
-def summe(a):
-    return a + 1 + a
+# def summe(a):
+#     return a + 1 + a
 
-# print(summe(a)) 
-# print(summe(a)) 
-# print(summe(a)) 
-# print(summe(a)) 
-# print(summe(a)) 
+# # print(summe(a)) 
+# # print(summe(a)) 
+# # print(summe(a)) 
+# # print(summe(a)) 
+# # print(summe(a)) 
 
-while a < 5:
-    if summe(a) == 5:
-        print("gewonnen")
-    else:
-        print(summe(a)) 
+# while a < 5:
+#     if summe(a) == 5:
+#         print("gewonnen")
+#     else:
+#         print(summe(a)) 
 
-    a = a + 1
+#     a = a + 1
+
+
+
+#####################################################################################
+vorname = "Mathias"
+nachname = "Cammerlander🟢ᵿ"
+alter = 32
+
+ausgabe = vorname + nachname + str(alter)
+
+print(ausgabe)
+
