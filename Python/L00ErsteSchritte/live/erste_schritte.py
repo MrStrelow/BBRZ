f7b07437d2339ddbed6d7934d0697bcfeb7ded21
MrStrelow <mathias.cammerlander@gmail.com>
2025-05-16 15:14:32 +0200

    asdf

--- Python/L00ErsteSchritte/live/erste_schritte.py
+++ Python/L00ErsteSchritte/live/erste_schritte.py
@@ -1,10 +1,21 @@
-print("hello me is da")
-print("hello me is da")
-print("hello me is da")
-print("hello me is da")
-print("hello me is da")
-print("hello me is da")
-print("hello me is da")
-print("hello me is da")
-print("hello me is da")
-print("hello me is da")
+# import time
+a = "hallo"
+a = 3.7
+a = 1
+
+def summe(a):
+    return a + 1 + a
+
+# print(summe(a)) 
+# print(summe(a)) 
+# print(summe(a)) 
+# print(summe(a)) 
+# print(summe(a)) 
+
+while a < 5:
+    if summe(a) == 5:
+        print("gewonnen")
+    else:
+        print(summe(a)) 
+
+    a = a + 1
