67947f959c9b6835691853d547f648493188469a
MrStrelow <mathias.cammerlander@gmail.com>
2025-05-21 14:33:05 +0200

    asfd

--- BITEC/modul_1_basic_concepts/src/lerneinheiten/L12Funktionen/live/A02FunktionenSchreiben.java
+++ BITEC/modul_1_basic_concepts/src/lerneinheiten/L12Funktionen/live/A02FunktionenSchreiben.java
@@ -2,6 +2,18 @@ package lerneinheiten.L12Funktionen.live;
 
 public class A02FunktionenSchreiben {
     public static void main(String[] args) {
+        // rufe die Funktion auf welche den Userinput aufruft.
+        // TODO: here
 
+        // rufe die Funktion auf welche Character zählt.
+        // TODO: here
     }
+
+    // schreib eine Funktion welche einen String entgegennimmt und die Character zählt.
+    // TODO: here
+
+    // schreib eine Funktion welche den User auffordert einen Satz,
+    // welcher mindestens einem Leerzeichen hat, einzugeben. Falls das nicht der Fall ist,
+    // frage nochmal nach.
+    // TODO: here
 }
