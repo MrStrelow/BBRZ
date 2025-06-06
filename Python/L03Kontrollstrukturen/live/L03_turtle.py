9eb5baa5c88f5df6a31aff0e647a889caf792091
MrStrelow <mathias.cammerlander@gmail.com>
2025-06-06 15:38:47 +0200

    asdf

--- Python/L03Kontrollstrukturen/live/L03_turtle.py
+++ Python/L03Kontrollstrukturen/live/L03_turtle.py
@@ -3,16 +3,28 @@ from turtle import *
 # # --- Vorbereitung # ---
 screen = Screen()
 screen.title("Übung 1.1: Verschachtelte Quadrate")
-turtle = Turtle()
-turtle.shape('turtle')
-turtle.speed(3) # Eine sichtbare Geschwindigkeit verwenden
+t = Turtle()
+t.shape("turtle")
+t.speed(3) # Eine sichtbare Geschwindigkeit verwenden
 
 # --- Äußeres Quadrat zeichnen (z.B. 200x200) # ---
-# TODO: implement me
+so_weit_gehe_ich = 200
+so_viel_drehe_ich_mich = 90
+
+t.penup()
+t.goto(x=-100, y=-100)
+t.pendown()
+t.forward(so_weit_gehe_ich)
+t.left(so_viel_drehe_ich_mich)
+t.forward(so_weit_gehe_ich)
+t.left(so_viel_drehe_ich_mich)
+t.forward(so_weit_gehe_ich)
+t.left(so_viel_drehe_ich_mich)
+t.forward(so_weit_gehe_ich)
 
 # --- Inneres Quadrat zeichnen (z.B. 100x100) # ---
 # TODO: implement me
 
 # --- Abschluss # ---
-turtle.hideturtle()
+# t.hideturtle()
 screen.exitonclick() # Warten, bis das Fenster per Klick geschlossen wird
