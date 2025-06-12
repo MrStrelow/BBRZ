dc7419cd57b6e14e331c0fcc825a135f857e8035
MrStrelow <mathias.cammerlander@gmail.com>
2025-06-12 09:35:18 +0200

    asf

---
+++ JET/modul_2_oop/ModulTest/VergangeneTests/ModulTest_AP10_2025_06/VorlageAufgabe2/visuals/implementation/Hamster/BigLeg/AngryBigLegHamsterVisuals.cs
@@ -0,0 +1,12 @@
+﻿using Hamster.Visuals.Representations;
+
+namespace Hamster.Visuals;
+
+// TODO: schlechter weil interface eher nicht schlank ist.
+// -> Ist jedoch gewünscht, denn wenn eine neue Representation eingeführt wird, muss diese für alle Implementiert werden.
+public class HungryBigLegHamsterVisuals : IVisuals
+{
+    // TODO: wir machen das in zukunft lazy - bedeutet erst wenn wir z.B. eine image representation brauchen, dann wird diese erzeugt.
+    public HtmlRepresentation HtmlRepresentation { get; init; } = new ("resources/BigLegHamster_hangry.png", 90);
+    public UnicodeRepresentation UnicoeRepresentation { get; init; } = new ("👿");
+}
