c1ad5636b358093dd8e89611e1b727bf8423cb02
MrStrelow <mathias.cammerlander@gmail.com>
2025-06-25 10:49:02 +0200

    asfd

--- BITEC/modul_2_object_orientation/src/lerneinheiten/L02KlassenUndMethoden/live/hamster/Main.java
+++ BITEC/modul_2_object_orientation/src/lerneinheiten/L02KlassenUndMethoden/live/hamster/Main.java
@@ -3,13 +3,15 @@ package lerneinheiten.L02KlassenUndMethoden.live.hamster;
 public class Main {
     public static void main(String[] args) {
         // variable spielfeld vom Typ Plane anlegen.
-        Plane spielfeld = new Plane(5);
-        System.out.println(spielfeld._size);
-        System.out.println(spielfeld._earthRepresentation);
+        Plane spielfeld = new Plane(6);
 
+        System.out.println(spielfeld._plane.length);
         while (true) {
             // hamster macht was
+            spielfeld.simulateHamster();
+
             // seedling macht was
+            spielfeld.simulateSeedling();
         }
     }
 }
--- BITEC/modul_2_object_orientation/src/lerneinheiten/L02KlassenUndMethoden/live/hamster/Plane.java
+++ BITEC/modul_2_object_orientation/src/lerneinheiten/L02KlassenUndMethoden/live/hamster/Plane.java
@@ -8,15 +8,35 @@ public class Plane {
     // Felder ("globale"-Variable)
     int _size;
     String _earthRepresentation = "🟫";
+    String[][] _plane;
 
     // Hat-Beziehungen
     ArrayList<Hamster> _hamsters = new ArrayList<>();
     ArrayList<Seedling> _seedlings = new ArrayList<>();
     // -----------------------------
     // Methoden
+    void simulateHamster() {
+
+    }
+
+    void simulateSeedling() {
+
+    }
+
     // Konstruktor
     Plane(int size) {
+        // felder initialisieren
         _size = size;
+        _plane = new String[_size][_size];
+
+        // ein wenig logik:
+        // * Darstellung der Plane (String[][]) mit dem earthsymbol auf allen stellen belegen
+
+        // * Hamster mit zufälliger Anzahl erstellen
+        // * diese der Liste hinzufügen
+
+        // * Seedlings mit zufälliger Anzahl erstellen
+        // * diese der Liste hinzufügen
     }
 }
 
