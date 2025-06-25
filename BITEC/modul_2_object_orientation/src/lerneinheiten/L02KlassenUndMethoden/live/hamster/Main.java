f83a11729f3bc7800e2166f3d59a83b69de2117d
MrStrelow <mathias.cammerlander@gmail.com>
2025-06-25 10:10:27 +0200

    asfd

--- BITEC/modul_2_object_orientation/src/lerneinheiten/L02KlassenUndMethoden/live/hamster/Main.java
+++ BITEC/modul_2_object_orientation/src/lerneinheiten/L02KlassenUndMethoden/live/hamster/Main.java
@@ -2,6 +2,14 @@ package lerneinheiten.L02KlassenUndMethoden.live.hamster;
 
 public class Main {
     public static void main(String[] args) {
+        // variable spielfeld vom Typ Plane anlegen.
+        Plane spielfeld = new Plane(5);
+        System.out.println(spielfeld._size);
+        System.out.println(spielfeld._earthRepresentation);
 
+        while (true) {
+            // hamster macht was
+            // seedling macht was
+        }
     }
 }
--- BITEC/modul_2_object_orientation/src/lerneinheiten/L02KlassenUndMethoden/live/hamster/Plane.java
+++ BITEC/modul_2_object_orientation/src/lerneinheiten/L02KlassenUndMethoden/live/hamster/Plane.java
@@ -1,9 +1,23 @@
 package lerneinheiten.L02KlassenUndMethoden.live.hamster;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Plane {
-    // Felder
+    // Felder ("globale"-Variable)
+    int _size;
+    String _earthRepresentation = "🟫";
+
     // Hat-Beziehungen
+    ArrayList<Hamster> _hamsters = new ArrayList<>();
+    ArrayList<Seedling> _seedlings = new ArrayList<>();
     // -----------------------------
     // Methoden
     // Konstruktor
+    Plane(int size) {
+        _size = size;
+    }
 }
+
+
