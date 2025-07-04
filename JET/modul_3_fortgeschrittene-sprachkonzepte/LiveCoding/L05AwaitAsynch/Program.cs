4fa0047fe470cb3126116d4ba50f6ce69a2b84de
MrStrelow <mathias.cammerlander@gmail.com>
2025-07-04 09:28:46 +0200

    asfd

--- JET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L05AwaitAsynch/Program.cs
+++ JET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L05AwaitAsynch/Program.cs
@@ -1,20 +1,29 @@
-﻿using Asynch;
+﻿using Synch;
 
 internal class Programm
 {
-    // Schritt 1 - void auf asynch Task umbenennen
-    public static async Task Main(string[] args)
+    static async Task Main(string[] args)
     {
         var userService = new UserService();
 
-        var newUser = new UserDTO { Id = 1, Name = "Lax Luster", Email = "lax@luster.lom" };
+        var newUser1 = new UserDTO { Id = 1, Name = "Lax Luster", Email = "lax@luster.lom" };
+        var newUser2 = new UserDTO { Id = 2, Name = "Nax Nuster", Email = "Nax@nuster.lom" };
         
-        await userService.SaveUserAsync(newUser);
+        await userService.SaveUserAsync(newUser1);
+        await userService.SaveUserAsync(newUser2);
         Console.WriteLine("--- Operation abgeschlossen, lade zur Kontrolle ---");
 
-        var loadedUser = await userService.LoadUserAsync();
+        var loadedUser1Task = userService.LoadUserByIdAsync(1);
+        var loadedUser2Task = userService.LoadUserByIdAsync(2);
 
-        Console.WriteLine($"Geladener Benutzer: {loadedUser.Name} ({loadedUser.Email})");
+        await Task.WhenAll(loadedUser1Task, loadedUser2Task); // warten auf den längsamsten task bis alle fertig sind.
+        //Task.WhenAny(loadedUser1, loadedUser2); // hören auf wenn ein task fertig ist.
+
+        var loadedUser1 = await loadedUser1Task;
+        var loadedUser2 = await loadedUser2Task;
+
+        Console.WriteLine($"Geladener Benutzer: {loadedUser1.Name} ({loadedUser1.Email})");
+        Console.WriteLine($"Geladener Benutzer: {loadedUser2.Name} ({loadedUser2.Email})");
         
         Console.WriteLine("\nDrücke eine Taste zum Beenden.");
         Console.ReadKey();
--- JET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L05AwaitAsynch/UserService.cs
+++ JET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L05AwaitAsynch/UserService.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace Asynch;
+namespace Synch;
 
 internal class UserService
 {
+    public async Task SaveUsersAsync(List<UserDTO> users)
+    {
+        Console.WriteLine("Speichere Benutzer... (dauert 2 Sekunden)");
+        //string json = JsonConvert.SerializeObject(users); // {"Id":1,"Name":"Lax Luster","Email":"lax@luster.lom"}
+        string json = JsonConvert.SerializeObject(users, Formatting.Indented); // schöner
+
+        // Simulieren wartezeit
+        await Task.Delay(2000);
+
+        await File.WriteAllTextAsync("../../../database.json", json);
+        Console.WriteLine("Benutzer erfolgreich gespeichert!");
+
+    }
+
     public async Task SaveUserAsync(UserDTO user)
     {
         Console.WriteLine("Speichere Benutzer... (dauert 2 Sekunden)");
-        //string json = JsonConvert.SerializeObject(user); // {"Id":1,"Name":"Lax Luster","Email":"lax@luster.lom"}
+        //string json = JsonConvert.SerializeObject(users); // {"Id":1,"Name":"Lax Luster","Email":"lax@luster.lom"}
         string json = JsonConvert.SerializeObject(user, Formatting.Indented); // schöner
 
         // Simulieren wartezeit
@@ -24,16 +37,39 @@ internal class UserService
 
     }
 
-    public async Task<UserDTO> LoadUserAsync()
+    public async Task<UserDTO?> LoadUserAsync()
     {
         Console.WriteLine("Lade Benutzer... (dauert 2 Sekunden)");
         string json = await File.ReadAllTextAsync("../../../database.json");
-        
+
         // Simulieren wartezeit
         await Task.Delay(2000);
 
         Console.WriteLine("Benutzer erfolgreich geladen!");
         return JsonConvert.DeserializeObject<UserDTO>(json);
+    }
 
+    public async Task<List<UserDTO>?> LoadUsersAsync()
+    {
+        Console.WriteLine("Lade Benutzer... (dauert 2 Sekunden)");
+        string json = await File.ReadAllTextAsync("../../../database.json");
+
+        // Simulieren wartezeit
+        await Task.Delay(2000);
+
+        Console.WriteLine("Benutzer erfolgreich geladen!");
+        return JsonConvert.DeserializeObject<List<UserDTO>>(json);
+    }
+
+    public async Task<UserDTO> LoadUserByIdAsync(int id)
+    {
+        Console.WriteLine($"Service: Suche Benutzer mit ID {id}...");
+
+        var users = await LoadUsersAsync();
+
+        if (users is not null)
+            return users.First(user => user.Id == id);
+        else
+            throw new Exception(":(");
     }
 }
