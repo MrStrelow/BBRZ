b5050ddfc676bf68f0a550f6272d6e251c8da0aa
MrStrelow <mathias.cammerlander@gmail.com>
2025-07-11 10:05:24 +0200

    asfd

---
+++ JET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L05.7Restauraunt/Program.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Restauraunt.Services;
+
+namespace Restauraunt;
+public class Programm
+{
+    static async Task Main(string[] args)
+    {
+        // 1. Logger init
+        Log.Logger = new LoggerConfiguration().
+            MinimumLevel.Debug().
+            WriteTo.Console().
+            CreateLogger();
+
+        Log.Information("Hello! Operation Fruehstueck initiated.");
+
+        // 2. Services initialisieren
+        var dishService = new DishService();
+        var menueService = new MenuService(dishService);
+        var billService = new BillService(menueService);
+
+        var analyticsService = new AnalyticsService();
+
+
+    }
+}
