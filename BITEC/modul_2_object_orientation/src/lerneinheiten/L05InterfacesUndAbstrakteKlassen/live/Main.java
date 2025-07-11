0a705af80d0c5b75c3904eefd92c1f1453dfcc77
MrStrelow <mathias.cammerlander@gmail.com>
2025-07-11 15:14:56 +0200

    asf

--- BITEC/modul_2_object_orientation/src/lerneinheiten/L05InterfacesUndAbstrakteKlassen/live/Main.java
+++ BITEC/modul_2_object_orientation/src/lerneinheiten/L05InterfacesUndAbstrakteKlassen/live/Main.java
@@ -1,22 +1,22 @@
-//package lerneinheiten.L05InterfacesUndAbstrakteKlassen.live;
-//
-//import lerneinheiten.L05InterfacesUndAbstrakteKlassen.live.klassen.Dreieck;
-//import lerneinheiten.L05InterfacesUndAbstrakteKlassen.live.klassen.Form;
-//import static lerneinheiten.L05InterfacesUndAbstrakteKlassen.live.klassen.Direction.*;
-//
-//public class Main {
-//    public static void main(String[] args) {
+package lerneinheiten.L05InterfacesUndAbstrakteKlassen.live;
+
+import lerneinheiten.L05InterfacesUndAbstrakteKlassen.live.klassen.Dreieck;
+import lerneinheiten.L05InterfacesUndAbstrakteKlassen.live.klassen.Form;
+import static lerneinheiten.L05InterfacesUndAbstrakteKlassen.live.klassen.Direction.*;
+
+public class Main {
+    public static void main(String[] args) {
 //        Dreieck triangle = new Dreieck(7, 7, "🌱", "👀", BOT_LEFT);
+
+//        Diamant diamiant = new Diamant(triangle);
+//        Diamant diamant = new Diamant(10, 10, "⬜", "👀");
 //
-////        Diamant diamiant = new Diamant(triangle);
-////        Diamant diamant = new Diamant(10, 10, "⬜", "👀");
-////
-////        ArrayList<Form> box = new ArrayList<>();
-////        box.add(diamiant);
-////        box.add(triangle);
-////
-////        for (Form form : box) {
-////            System.out.println(form);
-////        }
-//    }
-//}
+//        ArrayList<Form> box = new ArrayList<>();
+//        box.add(diamiant);
+//        box.add(triangle);
+//
+//        for (Form form : box) {
+//            System.out.println(form);
+//        }
+    }
+}
