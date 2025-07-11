b0ad305337bb14136609918cf7542f3e634a0f43
MrStrelow <mathias.cammerlander@gmail.com>
2025-07-11 11:33:40 +0200

    asfd

--- JET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L05.7Restauraunt/Services/Exception.cs
+++ JET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L05.7Restauraunt/Services/Exception.cs
@@ -8,6 +8,20 @@ namespace Restauraunt.Services.Exceptions;
 
 public class DishNotFoundException : Exception
 {
-    public
+    public DishNotFoundException(string message) : base(message) { }
 }
 
+public class OutOfStockException : Exception
+{
+    public OutOfStockException(string message) : base(message) { }
+}
+
+public class MenuPreparationException : Exception
+{
+    public MenuPreparationException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+public class OrderProcessingException : Exception
+{
+    public OrderProcessingException(string message, Exception innerException) : base(message, innerException) { }
+}
