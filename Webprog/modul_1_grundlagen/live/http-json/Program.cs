b9cfd1e5f8aa32b0ff06fd99ea8713eeb2e28583
MrStrelow <mathias.cammerlander@gmail.com>
2025-08-13 10:57:27 +0200

    asdf

--- Webprog/modul_1_grundlagen/live/http-json/Program.cs
+++ Webprog/modul_1_grundlagen/live/http-json/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var todos = new Dictionary<int, string>();
+todos.TryAdd(1, "einkaufen");
+todos.TryAdd(2, "pumpi");
+todos.TryAdd(3, "modul 2 test schreiben");
+todos.TryAdd(4, "modul 3 test schreiben");
+
+// Lege Endpoints fest - Welche http requests erkenne ich?
+// CRUD
+
+// Create
+app.MapPost("/", () => "Hello World!");
+
+// Read
 app.MapGet("/", () => "Hello World!");
+
+// Update
+app.MapPut("/", () => "Hello World!");
+
+// Delete
+app.MapDelete("/", () => "Hello World!");
 
 app.Run();
