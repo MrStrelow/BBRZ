11fa1da1975a9c4ee77fea63a811efa4ad18e200
MrStrelow <mathias.cammerlander@gmail.com>
2025-08-14 10:51:24 +0200

    asf

---
+++ JET/modul_4_orm/live/EFCoreFirstSteps/Data/DbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EFCoreFirstSteps.Model;
+
+namespace EFCoreFirstSteps.Data;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Dish> Dishes { get; set; }
+}
---
+++ JET/modul_4_orm/live/EFCoreFirstSteps/Model/Dish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreFirstSteps.Model;
+
+public class Dish
+{
+    // 4 Properties - Id, Name, Description, Price
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+}
