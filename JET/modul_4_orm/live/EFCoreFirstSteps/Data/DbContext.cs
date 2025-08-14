de0f664215c53e71408328c9f7b9e70f7702bb92
MrStrelow <mathias.cammerlander@gmail.com>
2025-08-14 10:57:47 +0200

    asf

--- JET/modul_4_orm/live/EFCoreFirstSteps/Data/DbContext.cs
+++ JET/modul_4_orm/live/EFCoreFirstSteps/Data/DbContext.cs
@@ -6,4 +6,13 @@ namespace EFCoreFirstSteps.Data;
 public class MyDbContext : DbContext
 {
     public DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Für login über den microsoft user
+        //optionsBuilder.UseSqlServer(@"Server=C432-LT-A7A3\SQLEXPRESS;Database=restaurant;Trusted_Connection=True;Trust Server Certificate=True");
+        
+        // Für login direkt auf dem sql server
+        optionsBuilder.UseSqlServer(@"Data Source=C432-LT-A7A3\SQLEXPRESS;Initial Catalog=fruehstueckdb;User ID=sa;Password=qwertz011235;Trust Server Certificate=True");
+    }
 }
