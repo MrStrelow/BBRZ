223d6c373e5ccec0bf8dc7bb031fdb5b21ee256f
MrStrelow <mathias.cammerlander@gmail.com>
2025-09-16 11:54:05 +0200

    asfd

--- C#/C#.NET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L01Bibliotheken/Program.cs
+++ C#/C#.NET/modul_3_fortgeschrittene-sprachkonzepte/LiveCoding/L01Bibliotheken/Program.cs
@@ -1,36 +1,46 @@
-﻿
-var sensoren = new List<Sensor>
+﻿using Serilog;
+using StringTools;
+
+public class Program
 {
-    new Sensor(Ort: "Wien", InBetriebSeit: 18, NächsteWartung: 1, AktuelleMessung: 26),
-    new Sensor(Ort: "Innsbruck", InBetriebSeit: 19, NächsteWartung: 2, AktuelleMessung: 29),
-    new Sensor(Ort: "Graz", InBetriebSeit: 20, NächsteWartung: 3, AktuelleMessung: 32),
-    new Sensor(Ort: "Innsbruck", InBetriebSeit: 21, NächsteWartung: 4, AktuelleMessung: -125124),
-    new Sensor(Ort: "Wien", InBetriebSeit: 22, NächsteWartung: 5, AktuelleMessung: -8744156),
-    new Sensor(Ort: "Wien", InBetriebSeit: 23, NächsteWartung: 6, AktuelleMessung: 25)
-};
+    static void Main(string[] args)
+    {
 
-// 1) Berechne höchste AktulleMessung aus Wien
-var sensorInWien = sensoren.Where(sensor => sensor.Ort == "Wien" && sensor.AktuelleMessung >= -50).Max(sensor => sensor.AktuelleMessung);
+        // EXTERNE BIBLIOTHEKEN
+        Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("../../../log.txt").CreateLogger();
 
-// 2) Sensoren aufsteigend sortiert, jedoch nur Ort und AktuelleMessung wird angezeigt
-var messungMitOrt = sensoren.OrderBy(s => s.InBetriebSeit).Select(s => new { s.Ort, s.AktuelleMessung });
+        Log.Verbose("Verbose: heitß sehr ausführlich ist es steht also hier viel text viel details");
+        Log.Debug("Debug: hier sind informationen fürs debugging. sehr viele also.");
+        Log.Information("Information");
+        Log.Warning("Warning");
 
-// 3) Durchschnittliche aktuelle Messung der Temperatur pro Ort
-var durchschnittProOrt = sensoren
-    .Where(s => s.AktuelleMessung > -50)
-    .GroupBy(s => s.Ort)
-    .Select(gruppe =>
-        new
+        try
         {
-            Ort = gruppe.Key,
-            DurchschnittlicheMessung = gruppe.Average(sensor => sensor.AktuelleMessung)
+            test();
         }
-    );
+        catch (Exception ex)
+        {
+            Log.Error($"Message: {ex.Message}\nStackTrace: {ex.StackTrace}");
+        }
 
-Console.WriteLine("Durchschnittliche aktuelle Messung der Temperatur pro Ort:");
-foreach (var ergebnis in durchschnittProOrt)
-{
-    Console.WriteLine($"- {ergebnis.Ort}: {Math.Round(ergebnis.DurchschnittlicheMessung, 2)}");
+        Log.Error("Error");
+        Log.Fatal("Fatal");
+        Log.CloseAndFlush();
+
+        // INTERNE BIBLIOTHEKEN (Projekte)
+        // Dsa untere soll nicht gehen.
+        //var anwendung = new FormatierungsLogik();
+        //anwendung.InGrossbuchstabenUmwandeln("asdf");
+        //anwendung.LeerzeichenEntfernen("asdf");
+
+        var andereAnwendung = new TextHelfer();
+        andereAnwendung.BeidesAufrufen("asdf");
+        
+
+    }
+
+    static void test()
+    {
+        throw new Exception("Fehler ist aufgetreten - sehr hilfreich 🙂");
+    }
 }
-
-public record Sensor(string Ort, int InBetriebSeit, int NächsteWartung, int AktuelleMessung);
