e748137b206f1e16d4410349551c378329b53733
MrStrelow <mathias.cammerlander@gmail.com>
2025-09-17 10:18:50 +0200

    asfd

--- C#/C#.ASP.NET/modul_1_grundlagen/live/MVC/Program.cs
+++ C#/C#.ASP.NET/modul_1_grundlagen/live/MVC/Program.cs
@@ -12,8 +12,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Todo}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Todo}/{action=Index}");
 
 
 app.Run();
