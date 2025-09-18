9c3d44aa1377ab4cf72b189dedd1cdec6503ef5d
MrStrelow <mathias.cammerlander@gmail.com>
2025-09-18 11:29:45 +0200

    asfd

---
+++ C#/C#.NET/modul_4_objekt_relationale_mapper/LiveCoding/Beziehungen/Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Beziehungen.Data;
+
+public class MyDbContext : DbContext
+{
+    // erstelle ein DBSet von Dishes als Eigenschaft
+    // erstelle ein DBSet von Ingredients als Eigenschaft
+    // erstelle ein DBSet von Preperationsteps als Eigenschaft
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=C432-LT-A7A3\SQLEXPRESS;Database=beziehungen_live;User ID=sa;Password=qwertz011235;Trust Server Certificate=True");
+    }
+}
---
+++ C#/C#.NET/modul_4_objekt_relationale_mapper/LiveCoding/Beziehungen/Model/PreparationStep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beziehungen.Model;
+
+internal class PreparationStep
+{
+    public int Id { get; set; }
+    public int StepNumber { get; set; }
+    public string Instruction { get; set; }
+    public Dish MyDish { get; set; } // -> bidirektionale Beziehung zu einem Dish, denn ein Dish hat mehrere PreperationSteps
+}
