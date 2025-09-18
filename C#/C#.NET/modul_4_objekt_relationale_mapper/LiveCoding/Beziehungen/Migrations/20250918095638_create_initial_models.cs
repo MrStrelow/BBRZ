115604e7115934572b1ca442c6fb8d00836a138d
MrStrelow <mathias.cammerlander@gmail.com>
2025-09-18 12:06:43 +0200

    asfd

---
+++ C#/C#.NET/modul_4_objekt_relationale_mapper/LiveCoding/Beziehungen/Migrations/20250918095638_create_initial_models.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beziehungen.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_initial_models : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
