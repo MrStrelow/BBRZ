38e3121414f407376af2e129fcca66576ce526c2
MrStrelow <mathias.cammerlander@gmail.com>
2025-10-02 10:02:41 +0200

    asdf

---
+++ C#/C#.ASP.NET/modul_1_grundlagen/L02MVC/UML-Dia/Visit.cs
@@ -0,0 +1,19 @@
+﻿namespace FruehstuecksBestellungMVC.Models;
+
+public class Visit
+{
+    public int Id { get; set; }
+    public DateTime EntryTime { get; set; }
+    // während des aufenthalts ist das ende noch nicht bekannt.
+    // Wir gehen davon aus, dass durch eine maske der besuch eingetragen wird,
+    // und nicht im nachhinein erst eingetragen wird. 
+    // Deshalb, DateTime? ExitTime und DateTime EntryTime
+    // (in der config noch mehr dazu)
+    public DateTime? ExitTime { get; set; } 
+    public int TableId { get; set; }
+    public Table? Table { get; set; }
+
+    public Bill? Bill { get; set; }
+    public ICollection<Customer> Customers { get; set; } = new List<Customer>();
+    public ICollection<Order> Orders { get; set; } = new List<Order>();
+}
