380190f5cdb5d6a0a0cfe77ed9219df19cc2f689
MrStrelow <mathias.cammerlander@gmail.com>
2026-01-23 11:03:49 +0100

    saf

---
+++ C#/C#.NET/modul_2_objektorientierung/LiveCoding/L00.1Properties/Program.cs
@@ -0,0 +1,41 @@
+﻿
+var helloBoy = new Hund
+{
+    Name = "hello",
+    Nachname = "Bello"
+};
+
+//var helloBoy = new Hund
+//(
+//    nachname: "bello", 
+//    name: "hello"
+//);
+
+helloBoy.Name = "hello"; // Schreibweise wie Feld
+helloBoy.Nachname = "hello"; // Schreibweise wie Feld - dürfen nicht set aufrufen durch init! (quasi private set aber geht init geht mit initializer)
+Console.WriteLine(helloBoy.Name);
+Console.WriteLine(helloBoy.Nachname);
+
+public class Hund
+{
+    public required string Name { 
+        get;
+        set //init
+        {
+            if (value is null) // set- guard
+                throw new Exception("test");
+
+            field = value;
+        }
+    } // Property: Definiert wie Get und Set Methode
+        
+    public string Nachname { get; init; } // Property: Definiert wie Get und Set Methode
+
+    //public Hund(string name, string nachname) // kein kosntruktor -> initialiser verwendenn
+    //{
+    //    Name = name;
+    //    Nachname = nachname;
+    //}
+}
+
+
