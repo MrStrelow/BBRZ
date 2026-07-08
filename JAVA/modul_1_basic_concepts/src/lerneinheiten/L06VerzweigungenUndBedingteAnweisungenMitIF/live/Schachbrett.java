5aa268f5750d3677f3855e76b3285f8e0c2bd087
MrStrelow <mathias.cammerlander@gmail.com>
2026-07-08 13:54:35 +0200

    asf

--- JAVA/modul_1_basic_concepts/src/lerneinheiten/L06VerzweigungenUndBedingteAnweisungenMitIF/live/Schachbrett.java
+++ JAVA/modul_1_basic_concepts/src/lerneinheiten/L06VerzweigungenUndBedingteAnweisungenMitIF/live/Schachbrett.java
@@ -5,7 +5,18 @@ public class Schachbrett {
         // Schachbrett zeichnen.
         // -> Problem immer zerlegen.
         // -> 1.) Ausgabe-Infrastruktur: gib ein Symbol der Gleichen Farbe in einer Zeile 5 mal hintereinander aus.
-        
 
+        System.out.print("🟩");
+        System.out.print("🟩");
+        System.out.print("🟩");
+        System.out.print("🟩");
+        System.out.print("🟩");
+
+        int maximaleHoehe = 5;
+        int maximaleBreite = 5;
+
+        for (int breite = 0; breite < maximaleBreite; breite++) {
+            System.out.print("🟩");
+        }
     }
 }
