7a640cebfccde2ebfa7c9c784e9f2aae20f919ec
MrStrelow <mathias.cammerlander@gmail.com>
2026-07-08 14:41:57 +0200

    asf

--- JAVA/modul_1_basic_concepts/src/lerneinheiten/L06VerzweigungenUndBedingteAnweisungenMitIF/live/Schachbrett.java
+++ JAVA/modul_1_basic_concepts/src/lerneinheiten/L06VerzweigungenUndBedingteAnweisungenMitIF/live/Schachbrett.java
@@ -13,7 +13,7 @@ public class Schachbrett {
 //        System.out.print("🟩");
 //        System.out.print("🟩");
 
-        // Erster Schritt: eine Zeile ausgeben.
+        // Erster Schritt: eine Zeile ausgeben fertig.
 //        int maximaleHoehe = 5;
 //        int maximaleBreite = 5;
 //
@@ -21,12 +21,30 @@ public class Schachbrett {
 //            System.out.print("🟩");
 //        }
 
+        // Zweiter Schritt: Brett fertig.
+//        int maximaleHoehe = 5;
+//        int maximaleBreite = 5;
+//
+//        for (int hoehe = 0; hoehe < maximaleHoehe; hoehe++) {
+//            for (int breite = 0; breite < maximaleBreite; breite++) {
+//                System.out.print("🟩");
+//            }
+//            System.out.println();
+//        }
+
         int maximaleHoehe = 5;
         int maximaleBreite = 5;
 
         for (int hoehe = 0; hoehe < maximaleHoehe; hoehe++) {
             for (int breite = 0; breite < maximaleBreite; breite++) {
-                System.out.print("🟩");
+                boolean bausteinGerade = breite % 2 == 0 && hoehe % 2 == 0;
+                boolean bausteinUngerade = breite % 2 == 1 && hoehe % 2 == 1;
+
+                if (bausteinUngerade || bausteinGerade) {
+                    System.out.print("🖥️");
+                } else {
+                    System.out.print("🌳");
+                }
             }
             System.out.println();
         }
