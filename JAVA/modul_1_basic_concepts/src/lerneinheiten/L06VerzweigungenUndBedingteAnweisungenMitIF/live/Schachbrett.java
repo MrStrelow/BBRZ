22cbd9d3e53b8d6be401adbd0f085c1a4266364f
MrStrelow <mathias.cammerlander@gmail.com>
2026-07-08 13:45:09 +0200

    asf

--- JAVA/modul_1_basic_concepts/src/lerneinheiten/L06VerzweigungenUndBedingteAnweisungenMitIF/live/Schachbrett.java
+++ JAVA/modul_1_basic_concepts/src/lerneinheiten/L06VerzweigungenUndBedingteAnweisungenMitIF/live/Schachbrett.java
@@ -2,6 +2,10 @@ package lerneinheiten.L06VerzweigungenUndBedingteAnweisungenMitIF.live;
 
 public class Schachbrett {
     public static void main(String[] args) {
+        // Schachbrett zeichnen.
+        // -> Problem immer zerlegen.
+        // -> 1.) Ausgabe-Infrastruktur: gib ein Symbol der Gleichen Farbe in einer Zeile 5 mal hintereinander aus.
+        
 
     }
 }
