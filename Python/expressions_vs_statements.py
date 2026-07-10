276bcd71f786d781ea3eff8d6f8d869bbc6c47c3
MrStrelow <mathias.cammerlander@gmail.com>
2026-07-10 14:49:04 +0200

    asf

--- Python/expressions_vs_statements.py
+++ Python/expressions_vs_statements.py
@@ -4,7 +4,7 @@
 #   * nicht in der Ergebnis liste vorhanden sein, wenn diese durch 9 teilbar sind.
 # Das ergebnis soll wieder eine Liste sein. 
 # Input: [4, 9, 11, 18, 20, 27]
-# Output: [8, 5, 36, 40]
+# Output: [8, 5, 40]
 
 
 # variante 1: Statements.
